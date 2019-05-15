50b9814e30f8a59fa0d9ba9ba83901f2028ef571
Weibe <weibe@pop-os.localdomain>
2019-05-15 08:53:52 -0300

    update check status build travis-ci

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@
 
 fails=0
 for i in $(grep -o 'travis.*\.svg' README.md); do
-  url=$(echo $i | sed "s/\//\/repositories\//" | sed "s/svg/json/")
-  curl -sq https://api.$url | grep 'last_build_result":0' > /dev/null
+  url=$(echo $i | sed "s/\//\/repositories\//" | sed "s/\.svg//")
+  curl -sq -H "Accept: application/json" https://api.$url | grep 'last_build_result":0' > /dev/null
   status=$?
   if [ "$status" != "0" ]; then
     fails=$((fails+${status}))
