40f9a6352c7321242e28f4f31ed58dc772b9dba5
Matt Klich <matt.klich@ibotta.com>
2019-05-30 21:39:59 -0600

    Travis API v2.1! Supports travis-ci .com and .org.

--- test.sh
+++ test.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
 
 fails=0
-skips=0
 total=0
 while read -r -a plugin; do
   total=$((total+1))
   travis_web_url="https://${plugin[0]}/${plugin[1]}"
-  if [[ ${plugin[0]} =~ .com ]]; then
-    skips=$((skips+1))
-    echo "Plugin build check skipped: ${travis_web_url} -- travis-ci.com (enterprise) checks are unsupported"
-    continue
-  fi
   url="https://api.${plugin[0]}/repos/${plugin[1]}/branches/${plugin[2]}"
-  curl -sq ${url} | grep '"state":"passed"' &> /dev/null
+  curl -sq -H 'Accept: application/vnd.travis-ci.2.1+json' ${url} | grep '"state":"passed"' &> /dev/null
   status=$?
   if [[ ${status} != 0 ]]; then
     fails=$((fails+1))
@@ -23,7 +17,6 @@ done < <(grep -o 'travis.*\.svg[^)]*' README.md | sed "s~https?://~~; s:/: :; s/
 echo
 echo "Plugins available: ${total}"
 echo "Plugin build checks passed: $((total-fails))"
-echo "Plugin build checks skipped: ${skips}"
 echo "Plugin build checks failed: ${fails}"
 
 exit ${fails}
