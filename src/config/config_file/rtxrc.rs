eef7cf5ad9563df997bc41651364a706556c8749
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-01-27 14:15:51 -0600

    rename aliases -> alias (what it should have been) (#17)

--- src/config/config_file/rtxrc.rs
+++ src/config/config_file/rtxrc.rs
@@ -100,7 +100,7 @@ impl RTXFile {
             "disable_plugin_short_name_repository" => {
                 self.settings.disable_plugin_short_name_repository = Some(self.parse_bool(k, v)?)
             }
-            "aliases" => self.settings.aliases = Some(self.parse_aliases(v)?),
+            "alias" => self.settings.aliases = Some(self.parse_aliases(v)?),
             "get_path" => {}
             _ => self.parse_plugin(k, v)?,
         };
