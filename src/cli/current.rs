274230e5aab03112ef47afe8fa41d276085a2d02
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-01-28 12:58:07 -0600

    [feat] made current better and not just an alias for `ls`

--- src/cli/current.rs
+++ src/cli/current.rs
@@ -1,74 +1,91 @@
 use color_eyre::eyre::Result;
 
 use crate::cli::command::Command;
-use crate::cli::Cli;
 use crate::config::Config;
 use crate::output::Output;
 
-/// Get the latest runtime version available for install
+/// Shows currently active, and installed runtime versions
 ///
-/// this exists for compatibility with asdf
-/// it just calls `rtx list --current` under the hood
+/// This is similar to `rtx list --current`, but this
+/// only shows the runtime and/or version so it's
+/// designed to fit into scripts more easily.
 #[derive(Debug, clap::Args)]
-#[clap(verbatim_doc_comment, after_long_help = AFTER_LONG_HELP, hide = true)]
+#[clap(verbatim_doc_comment, after_long_help = AFTER_LONG_HELP)]
 pub struct Current {
-    /// plugin to filter by
+    /// plugin to show versions of
     ///
     /// e.g.: ruby, nodejs
     #[clap()]
-    plugin: Option<Vec<String>>,
+    plugin: Option<String>,
 }
 
 impl Command for Current {
     fn run(self, config: Config, out: &mut Output) -> Result<()> {
-        let mut args = vec![
-            String::from("rtx"),
-            String::from("list"),
-            String::from("--current"),
-        ];
-        if let Some(plugins) = self.plugin {
-            for p in plugins {
-                args.push("-p".to_string());
-                args.push(p.to_string());
+        let plugin = match self.plugin {
+            Some(plugin_name) => {
+                let plugin = config.ts.find_plugin(&plugin_name);
+                if plugin.is_none() {
+                    warn!("Plugin {} is not installed", plugin_name);
+                    return Ok(());
+                }
+                Some(plugin.unwrap())
+            }
+            None => None,
+        };
+
+        for rtv in config.ts.list_current_versions() {
+            if !rtv.is_installed() {
+                let source = config.ts.get_source_for_plugin(&rtv.plugin.name).unwrap();
+                warn!(
+                    "{}@{} is specified in {}, but not installed",
+                    rtv.plugin.name, source, rtv.version
+                );
+            }
+            if let Some(plugin) = &plugin {
+                if plugin != &rtv.plugin {
+                    continue;
+                }
+                rtxprintln!(out, "{}", rtv.version);
+            } else {
+                rtxprintln!(out, "{}@{}", rtv.plugin.name, rtv.version);
             }
         }
-
-        Cli::new().run(config, &args, out)
+        Ok(())
     }
 }
 
 const AFTER_LONG_HELP: &str = r#"
 Examples:
+
   $ rtx current
-  -> shfmt      3.6.0                        (set by /Users/jdx/src/rtx/.tool-versions)
-     shellcheck 0.9.0 (missing)              (set by /Users/jdx/tool-versions)
-  -> nodejs     18.13.0                      (set by /Users/jdx/.tool-versions)
+  shfmt@3.6.0
+  shellcheck@0.9.0
+  nodejs@18.13.0
 
   $ rtx current nodejs
-  -> nodejs     18.13.0                      (set by /Users/jdx/.tool-versions)
+  18.13.0
 "#;
 
 #[cfg(test)]
 mod test {
-    use regex::Regex;
+    use insta::assert_snapshot;
 
     use crate::assert_cli;
+    use crate::cli::test::grep;
 
     #[test]
     fn test_current() {
-        assert_cli!("plugin", "add", "shellcheck");
         assert_cli!("install");
         let stdout = assert_cli!("current");
-        let re = Regex::new(r"-> shellcheck\s+0\.9\.0\s+").unwrap();
-        assert!(re.is_match(&stdout));
+        assert_snapshot!(grep(stdout, "shfmt"), @"shfmt@3.5.2");
     }
 
     #[test]
     fn test_current_with_runtimes() {
-        assert_cli!("plugin", "add", "shfmt");
         assert_cli!("install");
         let stdout = assert_cli!("current", "shfmt");
-        let re = Regex::new(r"-> shfmt\s+3\.5\.2\s+").unwrap();
-        assert!(re.is_match(&stdout));
+        assert_snapshot!(stdout, @r###"
+        3.5.2
+        "###);
     }
 }
