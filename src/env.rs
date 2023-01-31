916af0a185beb5e598750e95a065cb4541944ad1
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-01-30 18:31:42 -0600

    [chore] added debug output for early-exit to diagnose why GOROOT is not being set sometimes

--- src/env.rs
+++ src/env.rs
@@ -61,9 +61,11 @@ lazy_static! {
             "warn" => log::LevelFilter::Warn,
             "error" => log::LevelFilter::Error,
             _ => {
-                if var_is_true("RTX_DEBUG") {
+                if *RTX_TRACE {
+                    log::LevelFilter::Trace
+                } else if *RTX_DEBUG {
                     log::LevelFilter::Debug
-                } else if var_is_true("RTX_QUIET") {
+                } else if *RTX_QUIET {
                     log::LevelFilter::Error
                 } else {
                     log::LevelFilter::Info
@@ -78,6 +80,9 @@ lazy_static! {
     };
     pub static ref __RTX_DIR: Option<PathBuf> = var_os("__RTX_DIR").map(PathBuf::from);
     pub static ref __RTX_DIFF: EnvDiff = get_env_diff();
+    pub static ref RTX_QUIET: bool = var_is_true("RTX_QUIET");
+    pub static ref RTX_DEBUG: bool = var_is_true("RTX_DEBUG");
+    pub static ref RTX_TRACE: bool = var_is_true("RTX_TRACE");
     pub static ref PRISTINE_ENV: HashMap<String, String> =
         get_pristine_env(&__RTX_DIFF, vars().collect());
     pub static ref RTX_DEFAULT_TOOL_VERSIONS_FILENAME: String = if cfg!(test) {
