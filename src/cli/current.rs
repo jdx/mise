fcae62f28c495746e1dd8117a0bdf0562a7ca866
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-02-10 17:19:42 -0600

    feat: make `rtx current` output usable as a `.tool-versions` file

--- src/cli/current.rs
+++ src/cli/current.rs
@@ -1,8 +1,10 @@
 use color_eyre::eyre::Result;
+use std::sync::Arc;
 
 use crate::cli::command::Command;
 use crate::config::Config;
 use crate::output::Output;
+use crate::plugins::Plugin;
 
 /// Shows currently active, and installed runtime versions
 ///
@@ -21,35 +23,66 @@ pub struct Current {
 
 impl Command for Current {
     fn run(self, config: Config, out: &mut Output) -> Result<()> {
-        let plugin = match self.plugin {
-            Some(plugin_name) => {
-                let plugin = config.ts.find_plugin(&plugin_name);
-                if plugin.is_none() {
+        match &self.plugin {
+            Some(plugin_name) => match config.ts.find_plugin(plugin_name) {
+                Some(plugin) => self.one(&config, out, plugin),
+                None => {
                     warn!("Plugin {} is not installed", plugin_name);
-                    return Ok(());
+                    Ok(())
                 }
-                Some(plugin.unwrap())
-            }
-            None => None,
-        };
+            },
+            None => self.all(&config, out),
+        }
+    }
+}
 
-        for rtv in config.ts.list_current_versions() {
-            if !rtv.is_installed() {
-                let source = config.ts.get_source_for_plugin(&rtv.plugin.name).unwrap();
-                warn!(
-                    "{}@{} is specified in {}, but not installed",
-                    rtv.plugin.name, rtv.version, source
+impl Current {
+    fn one(&self, config: &Config, out: &mut Output, plugin: Arc<Plugin>) -> Result<()> {
+        if !plugin.is_installed() {
+            warn!("Plugin {} is not installed", plugin.name);
+            return Ok(());
+        }
+        let versions = config.ts.list_current_versions_by_plugin();
+        match versions.get(&plugin.name) {
+            Some(versions) => {
+                rtxprintln!(
+                    out,
+                    "{}",
+                    versions
+                        .iter()
+                        .map(|v| v.version.to_string())
+                        .collect::<Vec<_>>()
+                        .join(" ")
                 );
-                continue;
             }
-            if let Some(plugin) = &plugin {
-                if plugin != &rtv.plugin {
-                    continue;
+            None => {
+                warn!("Plugin {} does not have a version set", plugin.name);
+            }
+        };
+        Ok(())
+    }
+
+    fn all(&self, config: &Config, out: &mut Output) -> Result<()> {
+        for (plugin, versions) in config.ts.list_current_versions_by_plugin() {
+            for rtv in &versions {
+                if !rtv.is_installed() {
+                    let source = config.ts.get_source_for_plugin(&rtv.plugin.name).unwrap();
+                    warn!(
+                        "{}@{} is specified in {}, but not installed",
+                        &rtv.plugin.name, &rtv.version, &source
+                    );
                 }
-                rtxprintln!(out, "{}", rtv.version);
-            } else {
-                rtxprintln!(out, "{}@{}", rtv.plugin.name, rtv.version);
             }
+            rtxprintln!(
+                out,
+                "{} {}",
+                &plugin,
+                versions
+                    .iter()
+                    .map(|v| v.version.to_string())
+                    .collect::<Vec<_>>()
+                    .join(" ")
+            );
         }
         Ok(())
     }
@@ -58,13 +91,19 @@ impl Command for Current {
 const AFTER_LONG_HELP: &str = r#"
 Examples:
 
+  # outputs `.tool-versions` compatible format
   $ rtx current
-  shfmt@3.6.0
-  shellcheck@0.9.0
-  nodejs@18.13.0
+  python 3.11.0 3.10.0
+  shfmt 3.6.0
+  shellcheck 0.9.0
+  nodejs 18.13.0
 
   $ rtx current nodejs
   18.13.0
+
+  # can output multiple versions
+  $ rtx current python
+  3.11.0 3.10.0
 "#;
 
 #[cfg(test)]
@@ -78,7 +117,7 @@ mod test {
     fn test_current() {
         assert_cli!("install");
         let stdout = assert_cli!("current");
-        assert_snapshot!(grep(stdout, "shfmt"), @"shfmt@3.5.1");
+        assert_snapshot!(grep(stdout, "shfmt"), @"shfmt 3.5.1");
     }
 
     #[test]
