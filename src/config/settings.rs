3b4dceb7d3f74f94603ef13cb3e5b1e19aa76313
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-02-11 12:53:46 -0600

    bug: fix RTX_VERBOSE=1

--- src/config/settings.rs
+++ src/config/settings.rs
@@ -5,6 +5,7 @@ use indexmap::IndexMap;
 
 use crate::config::AliasMap;
 use crate::env;
+use crate::env::RTX_VERBOSE;
 use crate::plugins::PluginName;
 use crate::ui::prompt::is_tty;
 
@@ -30,7 +31,7 @@ impl Default for Settings {
             plugin_autoupdate_last_check_duration: Duration::from_secs(60 * 60 * 24 * 7),
             plugin_repository_last_check_duration: Duration::from_secs(60 * 60 * 24 * 7),
             aliases: IndexMap::new(),
-            verbose: !is_tty(),
+            verbose: *RTX_VERBOSE || !is_tty(),
         }
     }
 }
