da696fb1b4efeb6a8a357f3a6155f17421d9ddf2
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-02-18 23:12:03 -0600

    docs: document jobs flag

--- src/cli/render_help.rs
+++ src/cli/render_help.rs
@@ -395,6 +395,8 @@ always_keep_download = false        # deleted after install by default
 # (note: this isn't currently implemented but there are plans to add it: https://github.com/jdxcode/rtx/issues/128)
 plugin_autoupdate_last_check_duration = 10080 # (one week) set to 0 to disable updates
 
+jobs = 4 # number of plugins or runtimes to install in parallel. The default is `4`.
+
 verbose = false # see explanation under `RTX_VERBOSE`
 
 [alias.nodejs]
@@ -453,6 +455,10 @@ Output logs to a file.
 
 Same as `RTX_LOG_LEVEL` but for the log file output level. This is useful if you want
 to store the logs but not have them litter your display.
+
+#### `RTX_JOBS=1`
+
+Set the number plugins or runtimes to install in parallel. The default is `4`.
 
 #### `RTX_VERBOSE=1`
 
