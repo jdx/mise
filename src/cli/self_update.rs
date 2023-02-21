a3e6ac256abd9cd457d05211e7033853dbde905d
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-02-20 20:47:05 -0600

    chore: added aur release script to gh action

--- src/cli/self_update.rs
+++ src/cli/self_update.rs
@@ -1,10 +1,12 @@
 use color_eyre::Result;
 use console::style;
+use self_update::backends::github::Update;
 use self_update::cargo_crate_version;
 
 use crate::cli::command::Command;
 use crate::cli::version::{ARCH, OS};
 use crate::config::Config;
+use crate::env;
 use crate::output::Output;
 
 /// updates rtx itself
@@ -14,17 +16,23 @@ pub struct SelfUpdate {}
 
 impl Command for SelfUpdate {
     fn run(self, _config: Config, out: &mut Output) -> Result<()> {
-        let status = self_update::backends::github::Update::configure()
+        let current_version =
+            env::var("RTX_SELF_UPDATE_VERSION").unwrap_or(cargo_crate_version!().to_string());
+        let status = Update::configure()
             .repo_owner("jdxcode")
             .repo_name("rtx")
             .bin_name("rtx")
             .show_download_progress(true)
-            .current_version(cargo_crate_version!())
+            .current_version(&current_version)
             .target(&format!("{}-{}", *OS, *ARCH))
             .build()?
             .update()?;
-        let version = style(status.version()).bright().yellow();
-        rtxprintln!(out, "Updated rtx to {version}");
+        if status.updated() {
+            let version = style(status.version()).bright().yellow();
+            rtxprintln!(out, "Updated rtx to {version}");
+        } else {
+            rtxprintln!(out, "rtx is already up to date");
+        }
 
         Ok(())
     }
