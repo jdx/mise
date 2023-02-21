32c5cce6c9b3793640fb567ef204575b498fb124
Roland Schaer <roele33@hotmail.com>
2023-02-21 04:06:53 +0100

    feat: alias management commands (#25) (#159)
    * feat: alias management commands (#25)
    
    * feat: alias management commands (#25)
    - consolidate color utilities
    
    * feat: alias management commands (#25)
    - alphabetize commands
    
    * feat: alias management commands (#25)
    - regenerate help and completions
    
    * Update src/config/config_file/rtxrc.rs
    
    ---------
    
    Co-authored-by: Jeff Dickey <216188+jdxcode@users.noreply.github.com>

---
+++ src/cli/alias/set.rs
@@ -0,0 +1,59 @@
+use color_eyre::eyre::Result;
+use console::style;
+use indoc::formatdoc;
+use once_cell::sync::Lazy;
+
+use crate::cli::command::Command;
+use crate::config::config_file::ConfigFile;
+use crate::config::Config;
+use crate::output::Output;
+
+/// Add/update an alias for a plugin
+///
+/// This modifies the contents of ~/.config/rtx/config.toml
+#[derive(Debug, clap::Args)]
+#[clap(visible_aliases = ["add", "create"], after_long_help = AFTER_LONG_HELP.as_str(), verbatim_doc_comment)]
+pub struct AliasSet {
+    /// The plugin to set the alias for
+    pub plugin: String,
+    /// The alias to set
+    pub alias: String,
+    /// The value to set the alias to
+    pub value: String,
+}
+
+impl Command for AliasSet {
+    fn run(self, config: Config, _out: &mut Output) -> Result<()> {
+        let mut rtxrc = config.rtxrc;
+
+        rtxrc.set_alias(&self.plugin, &self.alias, &self.value);
+        rtxrc.save()
+    }
+}
+
+static AFTER_LONG_HELP: Lazy<String> = Lazy::new(|| {
+    formatdoc! {r#"
+    {}
+      $ rtx alias set nodejs lts/hydrogen 18.0.0
+    "#, style("Examples:").bold().underlined()}
+});
+
+#[cfg(test)]
+pub mod tests {
+    use insta::assert_snapshot;
+
+    use crate::assert_cli;
+
+    use crate::test::reset_config;
+
+    #[test]
+    fn test_alias_set() {
+        reset_config();
+        assert_cli!("alias", "set", "shfmt", "my/alias", "3.0");
+
+        let stdout = assert_cli!("aliases");
+        println!("stdout {}", stdout);
+        assert_snapshot!(stdout);
+        reset_config();
+    }
+}
---
+++ src/cli/alias/unset.rs
@@ -0,0 +1,58 @@
+use color_eyre::eyre::Result;
+use console::style;
+use indoc::formatdoc;
+use once_cell::sync::Lazy;
+
+use crate::cli::command::Command;
+use crate::config::config_file::ConfigFile;
+use crate::config::Config;
+use crate::output::Output;
+
+/// Clears an alias for a plugin
+///
+/// This modifies the contents of ~/.config/rtx/config.toml
+#[derive(Debug, clap::Args)]
+#[clap(visible_aliases=["rm", "remove", "delete", "del"], after_long_help = AFTER_LONG_HELP.as_str(), verbatim_doc_comment)]
+pub struct AliasUnset {
+    /// The plugin to remove the alias from
+    pub plugin: String,
+    /// The alias to remove
+    pub alias: String,
+}
+
+impl Command for AliasUnset {
+    fn run(self, config: Config, _out: &mut Output) -> Result<()> {
+        let mut rtxrc = config.rtxrc;
+        rtxrc.remove_alias(&self.plugin, &self.alias);
+        rtxrc.save()
+    }
+}
+
+static AFTER_LONG_HELP: Lazy<String> = Lazy::new(|| {
+    formatdoc! {r#"
+    {}
+      $ rtx alias unset nodejs lts/hydrogen
+    "#, style("Examples:").bold().underlined()}
+});
+
+#[cfg(test)]
+mod tests {
+    use insta::assert_snapshot;
+
+    use crate::assert_cli;
+
+    use crate::test::reset_config;
+
+    #[test]
+    fn test_settings_unset() {
+        reset_config();
+
+        assert_cli!("alias", "unset", "shfmt", "my/alias");
+
+        let stdout = assert_cli!("aliases");
+        assert_snapshot!(stdout, @r###"
+        "###);
+
+        reset_config();
+    }
+}
