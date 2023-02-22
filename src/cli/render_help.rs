745ab4d56e1715e4a50f46febe28cfb194626e23
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-02-22 15:06:55 -0600

    chore: Release rtx-cli version 1.15.0

--- src/cli/render_help.rs
+++ src/cli/render_help.rs
@@ -224,7 +224,7 @@ $ npx @jdxcode/rtx exec python@3.11 -- python some_script.py
 Download the latest release from [GitHub](https://github.com/jdxcode/rtx/releases).
 
 ```sh-session
-$ curl https://github.com/jdxcode/rtx/releases/download/v1.14.3/rtx-v1.14.3-linux-x64 | tar -xJv
+$ curl https://github.com/jdxcode/rtx/releases/download/v1.15.0/rtx-v1.15.0-linux-x64 | tar -xJv
 $ mv rtx/bin/rtx /usr/local/bin
 ```
 
