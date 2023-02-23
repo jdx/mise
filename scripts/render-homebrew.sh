4db68421d9ea1f37cf0be2c0ea388cb05848620f
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-02-22 21:11:51 -0600

    chore: fix homebrew formula shas

--- scripts/render-homebrew.sh
+++ scripts/render-homebrew.sh
@@ -3,9 +3,9 @@ set -euxo pipefail
 
 # shellcheck disable=SC2016
 RTX_VERSION=${RTX_VERSION#v*} \
-	RTX_CHECKSUM_LINUX_X86_64=$(grep linux-brew-x64.tar.xz "$RELEASE_DIR/SHASUMS256.txt" | cut -d ' ' -f1) \
-	RTX_CHECKSUM_LINUX_ARM64=$(grep linux-brew-arm64.tar.xz "$RELEASE_DIR/SHASUMS256.txt" | cut -d ' ' -f1) \
-	RTX_CHECKSUM_MACOS_X86_64=$(grep macos-brew-x64.tar.xz "$RELEASE_DIR/SHASUMS256.txt" | cut -d ' ' -f1) \
-	RTX_CHECKSUM_MACOS_ARM64=$(grep macos-brew-arm64.tar.xz "$RELEASE_DIR/SHASUMS256.txt" | cut -d ' ' -f1) \
+	RTX_CHECKSUM_LINUX_X86_64=$(grep "rtx-brew-$RTX_VERSION-linux-x64.tar.xz" "$RELEASE_DIR/$RTX_VERSION/SHASUMS256.txt" | cut -d ' ' -f1) \
+	RTX_CHECKSUM_LINUX_ARM64=$(grep "rtx-brew-$RTX_VERSION-linux-arm64.tar.xz" "$RELEASE_DIR/$RTX_VERSION/SHASUMS256.txt" | cut -d ' ' -f1) \
+	RTX_CHECKSUM_MACOS_X86_64=$(grep "rtx-brew-$RTX_VERSION-macos-x64.tar.xz" "$RELEASE_DIR/$RTX_VERSION/SHASUMS256.txt" | cut -d ' ' -f1) \
+	RTX_CHECKSUM_MACOS_ARM64=$(grep "rtx-brew-$RTX_VERSION-macos-arm64.tar.xz" "$RELEASE_DIR/$RTX_VERSION/SHASUMS256.txt" | cut -d ' ' -f1) \
 	envsubst '$RTX_VERSION,$RTX_CHECKSUM_LINUX_X86_64,$RTX_CHECKSUM_LINUX_ARM64,$RTX_CHECKSUM_MACOS_X86_64,$RTX_CHECKSUM_MACOS_ARM64' \
 	<rtx/packaging/homebrew/homebrew.rb
