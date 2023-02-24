fad268c37bf25f258dccd0ae524cc8be78e4d30f
Grant McLendon <grant@7mind.de>
2023-02-24 12:44:35 -0600

    Add script for fetching top starred asdf plugins

---
+++ scripts/query-top-plugins.fish
@@ -0,0 +1,36 @@
+#!/usr/bin/fish
+
+# Print out list of asdf plugins sorted by number of stars. Full list is
+# persisted to stargazer_count.txt
+
+if test -n (type -t gh)
+else
+    echo "Github CLI Missing! Aborting!"
+    exit 1
+end
+
+
+if test -d /tmp/asdf-plugins
+    set current_dir (pwd)
+    cd /tmp/asdf-plugins
+    git pull /tmp/asdf-plugins
+    cd $current_dir
+else
+    git clone --depth=1 git@github.com:asdf-vm/asdf-plugins.git /tmp/asdf-plugins
+end
+
+if test -e stargazer_count.txt
+    rm -rf stargazer_count.txt
+end
+
+
+for i in /tmp/asdf-plugins/plugins/*
+          cat $i | \
+                string split -f2 '=' | \
+                string trim | \
+                xargs -I '{}' gh repo view '{}' --json stargazerCount,name,owner | \
+                jq -r "[.stargazerCount,\"$(basename $i)\",\"$(string split '=' -f2 (cat $i))\"]|@tsv" >> stargazer_count.txt
+    end
+
+echo "$(cat stargazer_count.txt)" | sort -nr | uniq >stargazer_count.txt
+head -n 50 stargazer_count.txt
