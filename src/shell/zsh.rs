3260daf6bd09f00159ab05c3f1b04e0603055aeb
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-02-25 14:36:34 -0600

    bug: immediately call hook-env after activating (#204)
    Fixes #199

--- src/shell/zsh.rs
+++ src/shell/zsh.rs
@@ -34,6 +34,8 @@ impl Shell for Zsh {
             if [[ -z "${{chpwd_functions[(r)_rtx_hook]+1}}" ]]; then
               chpwd_functions=( _rtx_hook ${{chpwd_functions[@]}} )
             fi
+
+            _rtx_hook
             "#});
 
         out
