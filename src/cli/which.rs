3b2f66eb45c2a2c6704b887dcf140d90c70f1dcb
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-02-27 23:48:40 -0600

    feat: added `rtx which --version` and `--plugin` (#234)

--- src/cli/which.rs
+++ src/cli/which.rs
@@ -9,12 +9,20 @@ use crate::config::Config;
 use crate::output::Output;
 use crate::toolset::ToolsetBuilder;
 
-/// shows the plugin that a bin points to
+/// Shows the path that a bin name points to
 #[derive(Debug, clap::Args)]
 #[clap(verbatim_doc_comment, after_long_help = AFTER_LONG_HELP.as_str())]
 pub struct Which {
     #[clap()]
     pub bin_name: String,
+
+    /// Show the plugin name instead of the path
+    #[clap(long, conflicts_with = "version")]
+    pub plugin: bool,
+
+    /// Show the version instead of the path
+    #[clap(long, conflicts_with = "plugin")]
+    pub version: bool,
 }
 
 impl Command for Which {
@@ -23,8 +31,14 @@ impl Command for Which {
 
         match ts.which(&config.settings, &self.bin_name) {
             Some(rtv) => {
-                let path = rtv.which(&config.settings, &self.bin_name)?;
-                rtxprintln!(out, "{}", path.unwrap().display());
+                if self.version {
+                    rtxprintln!(out, "{}", rtv.version);
+                } else if self.plugin {
+                    rtxprintln!(out, "{}", rtv.plugin.name);
+                } else {
+                    let path = rtv.which(&config.settings, &self.bin_name)?;
+                    rtxprintln!(out, "{}", path.unwrap().display());
+                }
                 Ok(())
             }
             None => Err(eyre!("{} not found", self.bin_name)),
@@ -37,6 +51,10 @@ static AFTER_LONG_HELP: Lazy<String> = Lazy::new(|| {
     {}
       $ rtx which node
       /home/username/.local/share/rtx/installs/nodejs/18.0.0/bin/node
+      $ rtx which node --plugin
+      nodejs
+      $ rtx which node --version
+      18.0.0
     "#, style("Examples:").bold().underlined()}
 });
 
@@ -48,6 +66,22 @@ mod tests {
     fn test_which() {
         assert_cli!("global", "dummy@1.0.0");
         assert_cli_snapshot!("which", "dummy");
+        assert_cli!("global", "dummy@ref:master");
+        assert_cli!("uninstall", "dummy@1.0.0");
+    }
+
+    #[test]
+    fn test_which_plugin() {
+        assert_cli!("global", "dummy@1.0.0");
+        assert_cli_snapshot!("which", "--plugin", "dummy");
+        assert_cli!("global", "dummy@ref:master");
+        assert_cli!("uninstall", "dummy@1.0.0");
+    }
+
+    #[test]
+    fn test_which_version() {
+        assert_cli!("global", "dummy@1.0.0");
+        assert_cli_snapshot!("which", "--version", "dummy");
         assert_cli!("global", "dummy@ref:master");
         assert_cli!("uninstall", "dummy@1.0.0");
     }
