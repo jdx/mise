4c829434b4f739e919b349232055239452fb6e29
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-03-11 17:38:50 -0600

    feat: added --parseable and --json to `rtx ls` (#297)

--- src/toolset/tool_source.rs
+++ src/toolset/tool_source.rs
@@ -1,10 +1,13 @@
 use std::fmt::{Display, Formatter};
 use std::path::PathBuf;
 
+use indexmap::{indexmap, IndexMap};
+use serde_derive::Serialize;
+
 use crate::file::display_path;
 
 /// where a tool version came from (e.g.: .tool-versions)
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize)]
 pub enum ToolSource {
     ToolVersions(PathBuf),
     RtxToml(PathBuf),
@@ -25,6 +28,33 @@ impl Display for ToolSource {
     }
 }
 
+impl ToolSource {
+    pub fn as_json(&self) -> IndexMap<String, String> {
+        match self {
+            ToolSource::ToolVersions(path) => indexmap! {
+                "type".to_string() => ".tool-versions".to_string(),
+                "path".to_string() => path.to_string_lossy().to_string(),
+            },
+            ToolSource::RtxToml(path) => indexmap! {
+                "type".to_string() => ".rtx.toml".to_string(),
+                "path".to_string() => path.to_string_lossy().to_string(),
+            },
+            ToolSource::LegacyVersionFile(path) => indexmap! {
+                "type".to_string() => "legacy-version-file".to_string(),
+                "path".to_string() => path.to_string_lossy().to_string(),
+            },
+            ToolSource::Argument => indexmap! {
+                "type".to_string() => "argument".to_string(),
+            },
+            ToolSource::Environment(key, value) => indexmap! {
+                "type".to_string() => "environment".to_string(),
+                "key".to_string() => key.to_string(),
+                "value".to_string() => value.to_string(),
+            },
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use pretty_assertions::assert_str_eq;
@@ -41,10 +71,61 @@ mod tests {
         let ts = ToolSource::RtxToml(PathBuf::from("/home/user/.rtx.toml"));
         assert_str_eq!(ts.to_string(), "/home/user/.rtx.toml");
 
+        let ts = ToolSource::LegacyVersionFile(PathBuf::from("/home/user/.node-version"));
+        assert_str_eq!(ts.to_string(), "/home/user/.node-version");
+
         let ts = ToolSource::Argument;
         assert_str_eq!(ts.to_string(), "--runtime");
 
         let ts = ToolSource::Environment("RTX_NODEJS_VERSION".to_string(), "18".to_string());
         assert_str_eq!(ts.to_string(), "RTX_NODEJS_VERSION=18");
+    }
+
+    #[test]
+    fn test_tool_source_as_json() {
+        let ts = ToolSource::ToolVersions(PathBuf::from("/home/user/.test-tool-versions"));
+        assert_eq!(
+            ts.as_json(),
+            indexmap! {
+                "type".to_string() => ".tool-versions".to_string(),
+                "path".to_string() => "/home/user/.test-tool-versions".to_string(),
+            }
+        );
+
+        let ts = ToolSource::RtxToml(PathBuf::from("/home/user/.rtx.toml"));
+        assert_eq!(
+            ts.as_json(),
+            indexmap! {
+                "type".to_string() => ".rtx.toml".to_string(),
+                "path".to_string() => "/home/user/.rtx.toml".to_string(),
+            }
+        );
+
+        let ts = ToolSource::LegacyVersionFile(PathBuf::from("/home/user/.node-version"));
+        assert_eq!(
+            ts.as_json(),
+            indexmap! {
+                "type".to_string() => "legacy-version-file".to_string(),
+                "path".to_string() => "/home/user/.node-version".to_string(),
+            }
+        );
+
+        let ts = ToolSource::Argument;
+        assert_eq!(
+            ts.as_json(),
+            indexmap! {
+                "type".to_string() => "argument".to_string(),
+            }
+        );
+
+        let ts = ToolSource::Environment("RTX_NODEJS_VERSION".to_string(), "18".to_string());
+        assert_eq!(
+            ts.as_json(),
+            indexmap! {
+                "type".to_string() => "environment".to_string(),
+                "key".to_string() => "RTX_NODEJS_VERSION".to_string(),
+                "value".to_string() => "18".to_string(),
+            }
+        );
     }
 }
