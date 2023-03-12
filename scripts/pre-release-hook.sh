d095ec58a81242a5fcb1a394b9e1993a91c55dcd
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-03-12 16:57:45 -0500

    chore: run `cargo update` on release

--- scripts/pre-release-hook.sh
+++ scripts/pre-release-hook.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+if [[ "${NO_UPDATE:-}" == "1" ]]; then
+	echo "NO_UPDATE is set, skipping update"
+else
+	cargo update && git add Cargo.lock
+fi
+
 just render-mangen render-help
 
 ./scripts/update-shorthand-repo.sh
