f54f83d494b268d172389844902e3363cca76f2e
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-03-16 17:09:31 -0500

    chore: s3 publish

---
+++ scripts/publish-s3.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+cache_hour="max-age=3600,s-maxage=3600,public,immutable"
+cache_day="max-age=86400,s-maxage=86400,public,immutable"
+cache_week="max-age=604800,s-maxage=604800,public,immutable"
+
+./rtx/scripts/render-install.sh >"$RELEASE_DIR"/install.sh
+echo "$RTX_VERSION" | tr -d 'v' >"$RELEASE_DIR"/VERSION
+
+cp "$RELEASE_DIR/rtx-latest-linux-x64" "$RELEASE_DIR/rtx-latest-linux-amd64"
+cp "$RELEASE_DIR/rtx-latest-macos-x64" "$RELEASE_DIR/rtx-latest-macos-amd64"
+
+aws s3 cp "$RELEASE_DIR/$RTX_VERSION" "s3://rtx.pub/$RTX_VERSION/" --cache-control "$cache_week" --no-progress --recursive
+
+aws s3 cp "$RELEASE_DIR" "s3://rtx.pub/" --cache-control "$cache_hour" --no-progress --recursive --exclude "*" --include "rtx-latest-*"
+aws s3 cp "$RELEASE_DIR" "s3://rtx.pub/" --cache-control "$cache_hour" --no-progress --content-type "text/plain" --recursive --exclude "*" --include "SHASUMS*"
+aws s3 cp "$RELEASE_DIR/VERSION" "s3://rtx.pub/" --cache-control "$cache_hour" --no-progress --content-type "text/plain"
+aws s3 cp "$RELEASE_DIR/install.sh" "s3://rtx.pub/" --cache-control "$cache_hour" --no-progress --content-type "text/plain"
+
+aws s3 cp artifacts/rpm/rtx.repo s3://rtx.pub/rpm/ --cache-control "$cache_day" --no-progress
+aws s3 cp artifacts/rpm/packages/ s3://rtx.pub/rpm/packages/ --cache-control "$cache_week" --no-progress --recursive
+aws s3 cp artifacts/rpm/repodata/ s3://rtx.pub/rpm/repodata/ --cache-control "$cache_hour" --no-progress --recursive --exclude "*" --include "repomd.xml*"
+aws s3 cp artifacts/rpm/repodata/ s3://rtx.pub/rpm/repodata/ --cache-control "$cache_week" --no-progress --recursive --exclude "repomd.xml*"
+
+aws s3 cp artifacts/deb/pool/ s3://rtx.pub/deb/pool/ --cache-control "$cache_week" --no-progress --recursive
+aws s3 cp artifacts/deb/dists/ s3://rtx.pub/deb/dists/ --cache-control "$cache_hour" --no-progress --no-progress --recursive
+
+#aws cloudfront create-invalidation --distribution-id E166HHA8DY7YLW --paths \
+#	"/VERSION" \
+#	"/SHASUMS*" \
+#	"/install.sh" \
+#	"/rtx-latest-*" \
+#	"/rpm/repodata/*" \
+#	"/deb/dists/*"
