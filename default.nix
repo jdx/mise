2a0f11cea6e6cf951250ca73736febf9abad3402
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-03-17 15:23:04 -0500

    chore: Release rtx-cli version 1.24.0

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, fetchFromGitHub, rustPlatform, coreutils, bash, direnv, perl }:
 rustPlatform.buildRustPackage {
   pname = "rtx";
-  version = "1.23.10";
+  version = "1.24.0";
 
   src = lib.cleanSource ./.;
 
