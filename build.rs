9b4bbdb1fbe7f9f1565c742ad59f1787a899c7fd
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-03-22 07:49:48 -0500

    chore: fixing build.rs

--- build.rs
+++ build.rs
@@ -1,3 +1,4 @@
 fn main() {
     built::write_built_file().expect("Failed to acquire build-time information");
+    println!("cargo:rerun-if-changed=\"whateverYouLikeEvenAnAbsentDirectoryOrFile\"");
 }
