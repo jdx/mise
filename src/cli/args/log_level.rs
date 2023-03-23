28e0f32379c40de5d417e3d204e8a47f00af7ffb
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-03-23 10:19:35 -0500

    feat: added global --debug, and --trace flags (#380)

--- src/cli/args/log_level.rs
+++ src/cli/args/log_level.rs
@@ -1,5 +1,5 @@
 use clap::builder::ValueParser;
-use clap::Arg;
+use clap::{Arg, ArgAction};
 use log::{LevelFilter, ParseLevelError};
 use once_cell::sync::Lazy;
 
@@ -26,3 +26,27 @@ impl LogLevel {
 
 pub static DEFAULT_LOG_LEVEL: Lazy<String> =
     Lazy::new(|| env::RTX_LOG_LEVEL.to_string().to_lowercase());
+
+pub struct Debug;
+impl Debug {
+    pub fn arg() -> clap::Arg {
+        Arg::new("debug")
+            .long("debug")
+            .help("Sets log level to debug")
+            .hide(true)
+            .action(ArgAction::SetTrue)
+            .global(true)
+    }
+}
+
+pub struct Trace;
+impl Trace {
+    pub fn arg() -> clap::Arg {
+        Arg::new("trace")
+            .long("trace")
+            .help("Sets log level to trace")
+            .hide(true)
+            .action(ArgAction::SetTrue)
+            .global(true)
+    }
+}
