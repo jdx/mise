66b59e2af7b7079f6bf337ae0d1ebd9d6c210ed0
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-06-02 08:20:55 -0500

    chore: Release

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, stdenv, fetchFromGitHub, rustPlatform, coreutils, bash, direnv, perl }:
 rustPlatform.buildRustPackage {
   pname = "rtx";
-  version = "1.30.6";
+  version = "1.31.0";
 
   src = lib.cleanSource ./.;
 
