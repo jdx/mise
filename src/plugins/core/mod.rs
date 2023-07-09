b278c47e4cd2a5c2ea762f47b943e99bfd685f7d
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-07-09 11:03:02 -0500

    added core plugin common struct

--- src/plugins/core/mod.rs
+++ src/plugins/core/mod.rs
@@ -1,8 +1,13 @@
 use std::collections::BTreeMap;
+use std::path::PathBuf;
 use std::sync::Arc;
+use std::time::Duration;
 
 use once_cell::sync::Lazy;
 
+use crate::cache::CacheManager;
+use crate::dirs;
+use crate::env::RTX_EXE;
 pub use python::PythonPlugin;
 
 use crate::plugins::core::node::NodePlugin;
@@ -30,4 +35,25 @@ fn build_core_plugins(tools: Vec<Box<dyn Plugin>>) -> ToolMap {
             Arc::new(Tool::new(plugin.name().to_string(), plugin)),
         )
     }))
+}
+
+#[derive(Debug)]
+pub struct CorePlugin {
+    pub name: PluginName,
+    pub cache_path: PathBuf,
+    pub remote_version_cache: CacheManager<Vec<String>>,
+}
+
+impl CorePlugin {
+    pub fn new(name: PluginName) -> Self {
+        let cache_path = dirs::CACHE.join(&name);
+        let fresh_duration = Some(Duration::from_secs(60 * 60 * 12)); // 12 hours
+        Self {
+            remote_version_cache: CacheManager::new(cache_path.join("remote_versions.msgpack.z"))
+                .with_fresh_duration(fresh_duration)
+                .with_fresh_file(RTX_EXE.clone()),
+            name,
+            cache_path,
+        }
+    }
 }
