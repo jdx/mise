c01462518f16530f56a62694837020fc7256fd10
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-07-08 21:51:39 -0500

    chore: Release

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, stdenv, fetchFromGitHub, rustPlatform, coreutils, bash, direnv, perl }:
 rustPlatform.buildRustPackage {
   pname = "rtx";
-  version = "1.32.5";
+  version = "1.33.0";
 
   src = lib.cleanSource ./.;
 
--- packaging/rpm/rtx.spec
+++ packaging/rpm/rtx.spec
@@ -1,6 +1,6 @@
 Summary: Polyglot runtime manager
 Name: rtx
-Version: 1.32.5
+Version: 1.33.0
 Release: 1
 URL: https://github.com/jdxcode/rtx/
 Group: System
