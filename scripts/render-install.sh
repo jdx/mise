df05383208227862776877d07532ea1b26d001a8
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-07-30 18:20:25 -0500

    fixing standalone script to use the correct shasums

--- scripts/render-install.sh
+++ scripts/render-install.sh
@@ -3,9 +3,9 @@ set -euxo pipefail
 
 # shellcheck disable=SC2016
 RTX_VERSION=$RTX_VERSION \
-	RTX_CHECKSUM_LINUX_X86_64=$(grep linux-x64.tar.gz "$RELEASE_DIR/$RTX_VERSION/SHASUMS256.txt") \
-	RTX_CHECKSUM_LINUX_ARM64=$(grep linux-arm64.tar.gz "$RELEASE_DIR/$RTX_VERSION/SHASUMS256.txt") \
-	RTX_CHECKSUM_MACOS_X86_64=$(grep macos-x64.tar.gz "$RELEASE_DIR/$RTX_VERSION/SHASUMS256.txt") \
-	RTX_CHECKSUM_MACOS_ARM64=$(grep macos-arm64.tar.gz "$RELEASE_DIR/$RTX_VERSION/SHASUMS256.txt") \
+	RTX_CHECKSUM_LINUX_X86_64=$(grep "rtx-v.*linux-x64.tar.gz" "$RELEASE_DIR/$RTX_VERSION/SHASUMS256.txt") \
+	RTX_CHECKSUM_LINUX_ARM64=$(grep "rtx-v.*linux-arm64.tar.gz" "$RELEASE_DIR/$RTX_VERSION/SHASUMS256.txt") \
+	RTX_CHECKSUM_MACOS_X86_64=$(grep "rtx-v.*macos-x64.tar.gz" "$RELEASE_DIR/$RTX_VERSION/SHASUMS256.txt") \
+	RTX_CHECKSUM_MACOS_ARM64=$(grep "rtx-v.*macos-arm64.tar.gz" "$RELEASE_DIR/$RTX_VERSION/SHASUMS256.txt") \
 	envsubst '$RTX_VERSION,$RTX_CHECKSUM_LINUX_X86_64,$RTX_CHECKSUM_LINUX_ARM64,$RTX_CHECKSUM_MACOS_X86_64,$RTX_CHECKSUM_MACOS_ARM64' \
 	<rtx/packaging/standalone/install.envsubst
