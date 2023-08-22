c2728afbc4e99ebed1aa58d1a0cdff93117175b2
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-08-22 18:37:56 -0500

    fix race condition with cache file writing (#833)

---
+++ src/rand.rs
@@ -0,0 +1,10 @@
+use rand::distributions::Alphanumeric;
+use rand::Rng;
+
+pub fn random_string(length: usize) -> String {
+    rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(length)
+        .map(char::from)
+        .collect::<String>()
+}
