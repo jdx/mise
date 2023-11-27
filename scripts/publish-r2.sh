76f9a19fe3dcf7a3f20461902a6e66cc4410e7d9
Jeff Dickey <216188+jdx@users.noreply.github.com>
2023-11-26 22:19:14 -0600

    release: fix region env var

--- scripts/publish-r2.sh
+++ scripts/publish-r2.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-export AWS_DEFAULT_REGION=auto
+export AWS_REGION=auto
 export AWS_DEFAULT_OUTPUT=json
 export AWS_ENDPOINT_URL=https://6e243906ff257b965bcae8025c2fc344.r2.cloudflarestorage.com
 export AWS_ACCESS_KEY_ID=$CLOUDFLARE_ACCESS_KEY_ID
