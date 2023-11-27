818cb544b61052140361add8b8aca661c5df1c2e
Jeff Dickey <216188+jdx@users.noreply.github.com>
2023-11-26 20:32:30 -0600

    release: add shadow publishing to R2

---
+++ scripts/publish-r2.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+export AWS_DEFAULT_REGION=auto
+export AWS_DEFAULT_OUTPUT=json
+export AWS_ENDPOINT_URL=https://6e243906ff257b965bcae8025c2fc344.r2.cloudflarestorage.com
+export AWS_ACCESS_KEY_ID=$CLOUDFLARE_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$CLOUDFLARE_SECRET_ACCESS_KEY
+
+./rtx/scripts/publish-s3.sh || true
