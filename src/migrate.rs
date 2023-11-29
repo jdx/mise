d7e03c30fc39e4f68e00bc9f526640411a8af7cb
jdx <216188+jdx@users.noreply.github.com>
2023-11-29 13:16:14 -0600

    trusted-configs: move directory to data (#1021)
    I have had trouble deciding where to put this but I realized config is not a great place since it cannot really be shared on multiple machines

--- src/migrate.rs
+++ src/migrate.rs
@@ -2,14 +2,22 @@ use std::path::Path;
 
 use color_eyre::eyre::Result;
 
-use crate::{dirs, file};
+use crate::dirs::{CACHE, CONFIG, INSTALLS, PLUGINS, ROOT};
+use crate::file;
 
 pub fn run() -> Result<()> {
-    move_subdirs(&dirs::INSTALLS.join("nodejs"), &dirs::INSTALLS.join("node"))?;
-    move_subdirs(&dirs::INSTALLS.join("golang"), &dirs::INSTALLS.join("go"))?;
-    move_subdirs(&dirs::PLUGINS.join("nodejs"), &dirs::PLUGINS.join("node"))?;
-    move_subdirs(&dirs::PLUGINS.join("golang"), &dirs::PLUGINS.join("go"))?;
-    move_trusted_configs()?;
+    move_subdirs(&INSTALLS.join("nodejs"), &INSTALLS.join("node"))?;
+    move_subdirs(&INSTALLS.join("golang"), &INSTALLS.join("go"))?;
+    move_subdirs(&PLUGINS.join("nodejs"), &PLUGINS.join("node"))?;
+    move_subdirs(&PLUGINS.join("golang"), &PLUGINS.join("go"))?;
+    move_dirs(
+        &CACHE.join("trusted-configs"),
+        &ROOT.join("trusted-configs"),
+    )?;
+    move_dirs(
+        &CONFIG.join("trusted-configs"),
+        &ROOT.join("trusted-configs"),
+    )?;
 
     Ok(())
 }
@@ -33,9 +41,7 @@ fn move_subdirs(from: &Path, to: &Path) -> Result<()> {
     Ok(())
 }
 
-fn move_trusted_configs() -> Result<()> {
-    let from = dirs::CACHE.join("trusted-configs");
-    let to = dirs::CONFIG.join("trusted-configs");
+fn move_dirs(from: &Path, to: &Path) -> Result<()> {
     if from.exists() && !to.exists() {
         info!("migrating {} to {}", from.display(), to.display());
         file::create_dir_all(to.parent().unwrap())?;
