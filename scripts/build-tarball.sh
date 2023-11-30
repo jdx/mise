0f68d1471a176693183308aa43176496652166ba
Jeff Dickey <216188+jdx@users.noreply.github.com>
2023-11-29 19:11:21 -0600

    fish: change vendor_conf.d script name to rtx-activate.fish
    For some reason it does not work named "rtx.fish"

--- scripts/build-tarball.sh
+++ scripts/build-tarball.sh
@@ -71,7 +71,7 @@ cp "target/$RUST_TRIPLE/release/rtx" dist/rtx/bin/rtx
 cp README.md dist/rtx/README.md
 cp LICENSE dist/rtx/LICENSE
 cp {,dist/rtx/}man/man1/rtx.1
-cp {,dist/rtx/}share/fish/vendor_conf.d/rtx.fish
+cp {,dist/rtx/}share/fish/vendor_conf.d/rtx-activate.fish
 
 cd dist
 tar -cJf "$BASENAME.tar.xz" rtx
