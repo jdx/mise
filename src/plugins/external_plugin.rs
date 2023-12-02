jdx/mise

commit 5b8407b446bffc5dacfd0244805e8a6f003c4f4a

load installed/current plugins when installing plugins (#1047)
Possibly a cheap workaround for #393. It should make the following
possible:

    rtx install python
    rtx install poetry # poetry depends on python

If you were to just run `rtx install` and try to install python and
poetry at the same time it would not work, however.

src/plugins/external_plugin.rs
@@ -19,12 +19,13 @@ use crate::errors::Error::PluginNotInstalled;
 use crate::file::{display_path, remove_all};
 use crate::git::Git;
 use crate::hash::hash_to_str;
+use crate::install_context::InstallContext;
 use crate::plugins::external_plugin_cache::ExternalPluginCache;
 use crate::plugins::rtx_plugin_toml::RtxPluginToml;
 use crate::plugins::Script::{Download, ExecEnv, Install, ParseLegacyFile};
 use crate::plugins::{Plugin, PluginName, PluginType, Script, ScriptManager};
 use crate::timeout::run_with_timeout;
-use crate::toolset::{ToolVersion, ToolVersionRequest};
+use crate::toolset::{ToolVersion, ToolVersionRequest, Toolset};
 use crate::ui::multi_progress_report::MultiProgressReport;
 use crate::ui::progress_report::ProgressReport;
 use crate::ui::prompt;
@@ -237,15 +238,26 @@ impl ExternalPlugin {
         Ok(())
     }
 
-    fn fetch_bin_paths(&self, config: &Config, tv: &ToolVersion) -> Result<Vec<PathBuf>> {
+    fn fetch_bin_paths(
+        &self,
+        config: &Config,
+        ts: &Toolset,
+        tv: &ToolVersion,
+    ) -> Result<Vec<PathBuf>> {
         let list_bin_paths = self.plugin_path.join("bin/list-bin-paths");
         let bin_paths = if matches!(tv.request, ToolVersionRequest::System(_)) {
             Vec::new()
         } else if list_bin_paths.exists() {
-            let output = self
-                .script_man_for_tv(config, tv)
-                .cmd(&config.settings, &Script::ListBinPaths)
-                .read()?;
+            let mut sm = self.script_man_for_tv(config, tv);
+            for (t, tv) in ts.list_current_installed_versions(config) {
+                if t.name == self.name {
+                    continue;
+                }
+                for p in t.list_bin_paths(config, ts, &tv)? {
+                    sm.prepend_path(p);
+                }
+            }
+            let output = sm.cmd(&config.settings, &Script::ListBinPaths).read()?;
             output.split_whitespace().map(|f| f.to_string()).collect()
         } else {
             vec!["bin".into()]
@@ -256,9 +268,18 @@ impl ExternalPlugin {
             .collect();
         Ok(bin_paths)
     }
-    fn fetch_exec_env(&self, config: &Config, tv: &ToolVersion) -> Result<HashMap<String, String>> {
-        let script = self.script_man_for_tv(config, tv).get_script_path(&ExecEnv);
-        let ed = EnvDiff::from_bash_script(&script, &self.script_man_for_tv(config, tv).env)?;
+    fn fetch_exec_env(
+        &self,
+        config: &Config,
+        ts: &Toolset,
+        tv: &ToolVersion,
+    ) -> Result<HashMap<String, String>> {
+        let mut sm = self.script_man_for_tv(config, tv);
+        for p in ts.list_paths(config) {
+            sm.prepend_path(p);
+        }
+        let script = sm.get_script_path(&ExecEnv);
+        let ed = EnvDiff::from_bash_script(&script, &sm.env)?;
         let env = ed
             .to_patches()
             .into_iter()
@@ -595,22 +616,20 @@ impl Plugin for ExternalPlugin {
         exit(result.status.code().unwrap_or(1));
     }
 
-    fn install_version(
-        &self,
-        config: &Config,
-        tv: &ToolVersion,
-        pr: &ProgressReport,
-    ) -> Result<()> {
-        let run_script = |script| {
-            self.script_man_for_tv(config, tv)
-                .run_by_line(&config.settings, script, pr)
-        };
+    fn install_version(&self, ctx: &InstallContext) -> Result<()> {
+        let mut sm = self.script_man_for_tv(ctx.config, &ctx.tv);
 
-        if self.script_man_for_tv(config, tv).script_exists(&Download) {
-            pr.set_message("downloading");
+        for p in ctx.ts.list_paths(ctx.config) {
+            sm.prepend_path(p);
+        }
+
+        let run_script = |script| sm.run_by_line(&ctx.config.settings, script, &ctx.pr);
+
+        if sm.script_exists(&Download) {
+            ctx.pr.set_message("downloading");
             run_script(&Download)?;
         }
-        pr.set_message("installing");
+        ctx.pr.set_message("installing");
         run_script(&Install)?;
 
         Ok(())
@@ -624,12 +643,22 @@ impl Plugin for ExternalPlugin {
         Ok(())
     }
 
-    fn list_bin_paths(&self, config: &Config, tv: &ToolVersion) -> Result<Vec<PathBuf>> {
+    fn list_bin_paths(
+        &self,
+        config: &Config,
+        ts: &Toolset,
+        tv: &ToolVersion,
+    ) -> Result<Vec<PathBuf>> {
         self.cache
-            .list_bin_paths(config, self, tv, || self.fetch_bin_paths(config, tv))
+            .list_bin_paths(config, self, tv, || self.fetch_bin_paths(config, ts, tv))
     }
 
-    fn exec_env(&self, config: &Config, tv: &ToolVersion) -> Result<HashMap<String, String>> {
+    fn exec_env(
+        &self,
+        config: &Config,
+        ts: &Toolset,
+        tv: &ToolVersion,
+    ) -> Result<HashMap<String, String>> {
         if matches!(tv.request, ToolVersionRequest::System(_)) {
             return Ok(EMPTY_HASH_MAP.clone());
         }
@@ -639,7 +668,7 @@ impl Plugin for ExternalPlugin {
             return Ok(EMPTY_HASH_MAP.clone());
         }
         self.cache
-            .exec_env(config, self, tv, || self.fetch_exec_env(config, tv))
+            .exec_env(config, self, tv, || self.fetch_exec_env(config, ts, tv))
     }
 }
 

e2e/.gitignore
@@ -8,3 +8,5 @@
 ruby/Gemfile
 ruby/.ruby-version
 Library
+/pyproject.toml
+/poetry.lock

e2e/cd/test_bash
@@ -35,17 +35,17 @@ assert_path() {
 }
 
 test "$(node -v)" = "v20.0.0"
-assert_path "/root:ROOT/e2e/cwd:INSTALLS/node/20.0.0/bin:INSTALLS/tiny/3.1.0/bin:INSTALLS/shellcheck/0.9.0/bin:INSTALLS/shfmt/3.6.0/bin"
+assert_path "/root:ROOT/e2e/cwd:INSTALLS/node/20.0.0/bin:INSTALLS/python/3.12.0/bin:INSTALLS/tiny/3.1.0/bin:INSTALLS/poetry/1.7.1/bin:INSTALLS/shellcheck/0.9.0/bin:INSTALLS/shfmt/3.6.0/bin"
 assert "$FOO" "cd"
 
 cd 18 && _rtx_hook
 test "$(node -v)" = "v18.0.0"
-assert_path "/root:ROOT/e2e/cwd:INSTALLS/node/18.0.0/bin:INSTALLS/tiny/3.1.0/bin:INSTALLS/shellcheck/0.9.0/bin:INSTALLS/shfmt/3.6.0/bin"
+assert_path "/root:ROOT/e2e/cwd:INSTALLS/node/18.0.0/bin:INSTALLS/python/3.12.0/bin:INSTALLS/tiny/3.1.0/bin:INSTALLS/poetry/1.7.1/bin:INSTALLS/shellcheck/0.9.0/bin:INSTALLS/shfmt/3.6.0/bin"
 assert "$FOO" "18"
 
 cd .. && _rtx_hook
 test "$(node -v)" = "v20.0.0"
-assert_path "/root:ROOT/e2e/cwd:INSTALLS/node/20.0.0/bin:INSTALLS/tiny/3.1.0/bin:INSTALLS/shellcheck/0.9.0/bin:INSTALLS/shfmt/3.6.0/bin"
+assert_path "/root:ROOT/e2e/cwd:INSTALLS/node/20.0.0/bin:INSTALLS/python/3.12.0/bin:INSTALLS/tiny/3.1.0/bin:INSTALLS/poetry/1.7.1/bin:INSTALLS/shellcheck/0.9.0/bin:INSTALLS/shfmt/3.6.0/bin"
 
 rtx shell node@18.0.0 && _rtx_hook
 test "$(node -v)" = "v18.0.0"

e2e/config/.e2e.rtx.toml
@@ -5,7 +5,9 @@ env_path = ["/root", "./cwd"]
 FOO = "bar"
 
 [tools]
+python = "3.12.0"
 tiny = "latest"
+poetry = {version="1.7.1", pyproject="pyproject.toml"}
 #golang = {version="1.19.5", foo="bar"}
 
 [plugins]

e2e/config/pyproject.toml
@@ -1,0 +1,12 @@
+[tool.poetry]
+name = "poetry-test"
+version = "0.1.0"
+description = ""
+authors = ["rtx"]
+
+[tool.poetry.dependencies]
+python = "^3.12"
+
+[build-system]
+requires = ["poetry-core"]
+build-backend = "poetry.core.masonry.api"

e2e/run_test
@@ -23,7 +23,7 @@ setup_env() {
 setup_config_files() {
 	mkdir -p "$ROOT/e2e/cd/18"
 	cp "$ROOT/e2e/config/".e2e.* "$ROOT/e2e/"
-	cp "$ROOT/e2e/config/"{.node-version,.alternate-tool-versions,.test-env} "$ROOT/e2e/"
+	cp "$ROOT/e2e/config/"{.node-version,.alternate-tool-versions,.test-env,pyproject.toml} "$ROOT/e2e/"
 	cp "$ROOT/e2e/config/cd/".e2e.* "$ROOT/e2e/cd/"
 	cp "$ROOT/e2e/config/cd/18/".e2e.* "$ROOT/e2e/cd/18"
 }

e2e/test_doctor
@@ -11,5 +11,6 @@ assert() {
 	fi
 }
 
+rtx plugins install poetry && rtx i
 eval "$(rtx activate bash)" && _rtx_hook
 rtx doctor

e2e/test_local
@@ -27,7 +27,9 @@ env_path = [\"/root\", \"./cwd\"]
 FOO = \"bar\"
 
 [tools]
+python = \"3.12.0\"
 tiny = \"latest\"
+poetry = {version=\"1.7.1\", pyproject=\"pyproject.toml\"}
 #golang = {version=\"1.19.5\", foo=\"bar\"}
 
 [plugins]
@@ -42,7 +44,9 @@ env_path = [\"/root\", \"./cwd\"]
 FOO = \"bar\"
 
 [tools]
+python = \"3.12.0\"
 tiny = \"latest\"
+poetry = {version=\"1.7.1\", pyproject=\"pyproject.toml\"}
 shfmt = \"3.5.0\"
 #golang = {version=\"1.19.5\", foo=\"bar\"}
 
@@ -66,7 +70,9 @@ env_path = [\"/root\", \"./cwd\"]
 FOO = \"bar\"
 
 [tools]
+python = \"3.12.0\"
 tiny = \"latest\"
+poetry = {version=\"1.7.1\", pyproject=\"pyproject.toml\"}
 shfmt = \"3.6.0\"
 #golang = {version=\"1.19.5\", foo=\"bar\"}
 
@@ -87,7 +93,9 @@ env_path = [\"/root\", \"./cwd\"]
 FOO = \"bar\"
 
 [tools]
+python = \"3.12.0\"
 tiny = \"latest\"
+poetry = {version=\"1.7.1\", pyproject=\"pyproject.toml\"}
 #golang = {version=\"1.19.5\", foo=\"bar\"}
 
 [plugins]

commit 7fd2e6039f03bba59e09b68786eab375ed557997

fix deadlock
This was introduced in #1047

src/plugins/external_plugin.rs
@@ -241,22 +241,23 @@ impl ExternalPlugin {
     fn fetch_bin_paths(
         &self,
         config: &Config,
-        ts: &Toolset,
+        _ts: &Toolset,
         tv: &ToolVersion,
     ) -> Result<Vec<PathBuf>> {
         let list_bin_paths = self.plugin_path.join("bin/list-bin-paths");
         let bin_paths = if matches!(tv.request, ToolVersionRequest::System(_)) {
             Vec::new()
         } else if list_bin_paths.exists() {
-            let mut sm = self.script_man_for_tv(config, tv);
-            for (t, tv) in ts.list_current_installed_versions(config) {
-                if t.name == self.name {
-                    continue;
-                }
-                for p in t.list_bin_paths(config, ts, &tv)? {
-                    sm.prepend_path(p);
-                }
-            }
+            let sm = self.script_man_for_tv(config, tv);
+            // TODO: find a way to enable this without deadlocking
+            // for (t, tv) in ts.list_current_installed_versions(config) {
+            //     if t.name == self.name {
+            //         continue;
+            //     }
+            //     for p in t.list_bin_paths(config, ts, &tv)? {
+            //         sm.prepend_path(p);
+            //     }
+            // }
             let output = sm.cmd(&config.settings, &Script::ListBinPaths).read()?;
             output.split_whitespace().map(|f| f.to_string()).collect()
         } else {