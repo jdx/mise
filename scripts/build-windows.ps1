15b7731ac748daee0525a5239fa25be0be8e4d19
Jeff Dickey <216188+jdx@users.noreply.github.com>
2023-12-06 18:58:58 -0600

    lint

--- scripts/build-windows.ps1
+++
@@ -1,5 +0,0 @@
-Get-ChildItem target\release
-New-Item dist\rtx\bin -ItemType Directory -ea 0
-Copy-Item target\release\rtx.exe dist\rtx\bin\rtx.exe
-$Env:RTX_VERSION = (cargo get version --pretty)
-Compress-Archive -Path dist\rtx -DestinationPath dist\rtx-$env:RTX_VERSION-windows-x64.zip
