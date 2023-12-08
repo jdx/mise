0ed57f9d4c25969f1ce11cfe10d223141cd5ce86
jdx <216188+jdx@users.noreply.github.com>
2023-12-08 11:17:38 -0600

    standalone: bug fix (#1130)
    * standalone: bug fix
    
    * release fixes
    
    * release fixes
    
    * release fixes

--- scripts/release.sh
+++ scripts/release.sh
@@ -54,6 +54,11 @@ gpg --clearsign -u 408B88DB29DDE9E0 <SHASUMS512.txt >SHASUMS512.asc
 popd
 
 ./rtx/scripts/render-install.sh >"$RELEASE_DIR"/install.sh
+chmod +x "$RELEASE_DIR"/install.sh
+shellcheck "$RELEASE_DIR"/install.sh
+# TODO: figure out how to test this
+# "$RELEASE_DIR"/install.sh
+#~/.local/share/rtx/bin/rtx -v
 gpg -u 408B88DB29DDE9E0 --output "$RELEASE_DIR"/install.sh.sig --sign "$RELEASE_DIR"/install.sh
 
 if [[ "$DRY_RUN" != 1 ]]; then
