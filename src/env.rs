f56969091da8d4d04c37572e94ddac25d7d8274a
Jean-Charles Sisk <jasisk@gmail.com>
2023-12-08 12:18:19 -0500

    (nodejs plugin) add support for automatically shimming package managers with `corepack enable` (#1118)
    * feat: support RTX_NODE_COREPACK_ENABLE
    
    * chore: lint e2e/test_nodejs
    
    * refactor(node): RTX_NODE_COREPACK_ENABLE -> RTX_NODE_COREPACK
    
    * refactor(node): remove unnecessary `test_corepack`
    
    ---------
    
    Co-authored-by: jdx <216188+jdx@users.noreply.github.com>

--- src/env.rs
+++ src/env.rs
@@ -167,6 +167,7 @@ pub static RTX_NODE_DEFAULT_PACKAGES_FILE: Lazy<PathBuf> = Lazy::new(|| {
         HOME.join(".default-npm-packages")
     })
 });
+pub static RTX_NODE_COREPACK: Lazy<bool> = Lazy::new(|| var_is_true("RTX_NODE_COREPACK"));
 pub static NVM_DIR: Lazy<PathBuf> =
     Lazy::new(|| var_path("NVM_DIR").unwrap_or_else(|| HOME.join(".nvm")));
 pub static NODENV_ROOT: Lazy<PathBuf> =
