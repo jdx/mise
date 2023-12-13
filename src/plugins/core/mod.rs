6a536615c9927606067092b1a3e4ca24055a4c47
Jeff Dickey <216188+jdx@users.noreply.github.com>
2023-12-12 22:33:48 -0600

    deno|bun: make non-experimental

--- src/plugins/core/mod.rs
+++ src/plugins/core/mod.rs
@@ -39,6 +39,8 @@ pub type PluginMap = BTreeMap<String, Arc<dyn Plugin>>;
 
 pub static CORE_PLUGINS: Lazy<PluginMap> = Lazy::new(|| {
     let plugins: Vec<Arc<dyn Plugin>> = vec![
+        Arc::new(BunPlugin::new()),
+        Arc::new(DenoPlugin::new()),
         Arc::new(GoPlugin::new()),
         Arc::new(JavaPlugin::new()),
         if *RTX_NODE_BUILD == Some(true) {
@@ -57,8 +59,6 @@ pub static CORE_PLUGINS: Lazy<PluginMap> = Lazy::new(|| {
 
 pub static EXPERIMENTAL_CORE_PLUGINS: Lazy<PluginMap> = Lazy::new(|| {
     let plugins: Vec<Arc<dyn Plugin>> = vec![
-        Arc::new(BunPlugin::new()),
-        Arc::new(DenoPlugin::new()),
         Arc::new(ErlangPlugin::new()),
     ];
     plugins
