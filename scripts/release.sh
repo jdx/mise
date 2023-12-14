a8e8d88a54b70845af9a021d9d36c62c53a0a270
jdx <216188+jdx@users.noreply.github.com>
2023-12-14 04:34:35 -0600

    CI: consolidate GH action cache (#1181)
    * CI: consolidate GH action cache
    
    * CI: consolidate GH action cache
    
    * Commit from GitHub Actions (test)
    
    * CI: test run formatting
    
    * e2e
    
    * ci
    
    * ci
    
    * poetry
    
    * e2e
    
    * poetry
    
    * e2e
    
    * poetry
    
    * e2e
    
    * make more e2e tests slow
    
    ---------
    
    Co-authored-by: rtx[bot] <123107610+rtx-vm@users.noreply.github.com>

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+echo "::group::Setup"
 git config --global user.name rtx-vm
 git config --global user.email 123107610+rtx-vm@users.noreply.github.com
 
@@ -11,6 +12,7 @@ export BASE_DIR RTX_VERSION RELEASE_DIR
 rm -rf "${RELEASE_DIR:?}/$RTX_VERSION"
 mkdir -p "$RELEASE_DIR/$RTX_VERSION"
 
+echo "::group::Build"
 find artifacts -name 'tarball-*' -exec sh -c '
   target=${1#artifacts/tarball-}
   cp "artifacts/tarball-$target/"*.tar.gz "$RELEASE_DIR/$RTX_VERSION"
@@ -37,6 +39,7 @@ for platform in "${platforms[@]}"; do
   cp -v rtx/bin/rtx "$RELEASE_DIR/$RTX_VERSION/rtx-$RTX_VERSION-$platform"
 done
 
+echo "::group::Checksums"
 pushd "$RELEASE_DIR"
 echo "$RTX_VERSION" | tr -d 'v' >VERSION
 cp rtx-latest-linux-x64 rtx-latest-linux-amd64
@@ -54,18 +57,22 @@ gpg --clearsign -u 408B88DB29DDE9E0 <SHASUMS256.txt >SHASUMS256.asc
 gpg --clearsign -u 408B88DB29DDE9E0 <SHASUMS512.txt >SHASUMS512.asc
 popd
 
+echo "::group::install.sh"
 ./rtx/scripts/render-install.sh >"$RELEASE_DIR"/install.sh
 chmod +x "$RELEASE_DIR"/install.sh
 shellcheck "$RELEASE_DIR"/install.sh
 gpg -u 408B88DB29DDE9E0 --output "$RELEASE_DIR"/install.sh.sig --sign "$RELEASE_DIR"/install.sh
 
 if [[ "$DRY_RUN" != 1 ]]; then
+  echo "::group::Publish npm @jdxcode/rtx"
   NPM_PREFIX=@jdxcode/rtx ./rtx/scripts/release-npm.sh
+  echo "::group::Publish npm rtx-cli"
   NPM_PREFIX=rtx-cli ./rtx/scripts/release-npm.sh
-  #AWS_S3_BUCKET=rtx.jdx.dev ./rtx/scripts/publish-s3.sh
+  echo "::group::Publish r2"
   ./rtx/scripts/publish-r2.sh
 fi
 
+echo "::group::Publish homebrew"
 ./rtx/scripts/render-homebrew.sh >homebrew-tap/rtx.rb
 pushd homebrew-tap
 git add . && git commit -m "rtx ${RTX_VERSION#v}"
