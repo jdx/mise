9cbb2512e75c3bea6fa6ac70896339345d6ec539
Jeff Dickey <216188+jdx@users.noreply.github.com>
2023-12-28 09:52:18 -0600

    set github url

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/jdx/rtx' }
     ]
   }
 })
