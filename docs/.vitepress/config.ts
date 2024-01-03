abade67d44469d005cf4b681a898b29530ae7bff
Jeff Dickey <216188+jdx@users.noreply.github.com>
2024-01-02 18:12:25 -0600

    nav

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -9,7 +9,6 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Getting Started', link: '/getting-started' },
       { text: 'Dev Tools', link: '/dev-tools' },
       { text: 'Environments', link: '/environments' },
       { text: 'Tasks', link: '/tasks/' },
