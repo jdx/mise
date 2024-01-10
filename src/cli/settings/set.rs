23eef59b8cc09ed3cbf5c649d47e263cda74e588
jdx <216188+jdx@users.noreply.github.com>
2024-01-10 16:07:42 -0600

    fix test suite on alpine (#1433)
    * fix test suite on alpine
    
    * fix test suite on alpine

--- src/cli/settings/set.rs
+++ src/cli/settings/set.rs
@@ -119,7 +119,7 @@ pub mod tests {
         paranoid = false
         plugin_autoupdate_last_check_duration = "7d"
         python_compile = false
-        python_default_packages_file = null
+        python_default_packages_file = "~/.default-python-packages"
         python_patch_url = null
         python_patches_directory = null
         python_precompiled_arch = null
