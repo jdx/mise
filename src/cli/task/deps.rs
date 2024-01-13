3926ef790e564cc26ece56e03b4f1720bd83f028
Roland Schaer <roele33@hotmail.com>
2024-01-13 20:24:23 +0100

    feat: add command to print task dependency tree (#1440)
    * feat: add command to print task dependency tree
    
    * move Deps struct to tasks.rs
    
    * remove crate ptree and implement rendering of a DiGraph as tree
    
    * move tree rendition types to ui/tree.rs
    
    * dont swallow error
    
    * fix inconcise doc comment
    
    * fix rebase issues
    
    * add a config.task_with_aliases function and remove filtering
    
    * add unit tests for tasks deps command
    
    * fix conditional for EndBug/add-and-commit@v9 step in test job
    
    * address linting issues

---
+++ src/cli/task/deps.rs
@@ -0,0 +1,197 @@
+use crate::{
+    config::{Config, Settings},
+    task::{Deps, Task},
+    ui::{style, tree::print_tree},
+};
+use console::style;
+use eyre::Result;
+use itertools::Itertools;
+use petgraph::dot::Dot;
+
+/// [experimental] Display a tree visualization of a dependency graph
+#[derive(Debug, clap::Args)]
+#[clap(verbatim_doc_comment, after_long_help = AFTER_LONG_HELP)]
+pub struct TaskDeps {
+    /// Tasks to get dependencies for
+    /// Can specify multiple tasks by separating with spaces
+    /// e.g.: mise task deps task1 task2
+    #[clap(verbatim_doc_comment)]
+    pub tasks: Option<Vec<String>>,
+
+    /// Display dependencies in DOT format
+    #[clap(long, alias = "dot", verbatim_doc_comment)]
+    pub dot: bool,
+}
+
+impl TaskDeps {
+    pub fn run(self) -> Result<()> {
+        let config = Config::try_get()?;
+        let settings = Settings::try_get()?;
+        settings.ensure_experimental()?;
+
+        let tasks = if self.tasks.is_none() {
+            self.get_all_tasks(&config)?
+        } else {
+            self.get_task_lists(&config)?
+        };
+
+        if self.dot {
+            self.print_deps_dot(&config, tasks)?;
+        } else {
+            self.print_deps_tree(&config, tasks)?;
+        }
+
+        Ok(())
+    }
+
+    fn get_all_tasks(&self, config: &Config) -> Result<Vec<Task>> {
+        config
+            .tasks()
+            .iter()
+            .map(|(_, t)| t)
+            .sorted()
+            .filter(|t| !t.hide)
+            .map(|t| Ok(t.to_owned()))
+            .collect()
+    }
+
+    fn get_task_lists(&self, config: &Config) -> Result<Vec<Task>> {
+        let tasks = self.tasks.as_ref().map(|t| {
+            t.iter()
+                .sorted()
+                .map(|tn| match config.tasks().get(tn) {
+                    Some(task) => Ok(task.clone()),
+                    None => Err(self.err_no_task(config, tn.as_str())),
+                })
+                .collect::<Result<Vec<Task>>>()
+        });
+        match tasks {
+            Some(Ok(tasks)) => Ok(tasks),
+            Some(Err(e)) => Err(e),
+            None => Ok(vec![]),
+        }
+    }
+
+    ///
+    /// Print dependencies as a tree
+    ///
+    /// Example:
+    /// ```
+    /// task1
+    /// ├─ task2
+    /// │  └─ task3
+    /// └─ task4
+    /// task5
+    /// ```
+    ///
+    fn print_deps_tree(&self, config: &Config, tasks: Vec<Task>) -> Result<()> {
+        let deps = Deps::new(config, tasks.clone())?;
+        // filter out nodes that are not selected
+        let start_indexes = deps.graph.node_indices().filter(|&idx| {
+            let task = &deps.graph[idx];
+            tasks.iter().any(|t| t.name == task.name)
+        });
+        // iterate over selected graph nodes and print tree
+        for idx in start_indexes {
+            print_tree(&(&deps.graph, idx));
+        }
+        Ok(())
+    }
+
+    ///
+    /// Print dependencies in DOT format
+    ///
+    /// Example:
+    /// ```
+    /// digraph {
+    ///  1 [label = "task1"]
+    ///  2 [label = "task2"]
+    ///  3 [label = "task3"]
+    ///  4 [label = "task4"]
+    ///  5 [label = "task5"]
+    ///  1 -> 2 [ ]
+    ///  2 -> 3 [ ]
+    ///  1 -> 4 [ ]
+    /// }
+    /// ```
+    //
+    fn print_deps_dot(&self, config: &Config, tasks: Vec<Task>) -> Result<()> {
+        let deps = Deps::new(config, tasks)?;
+        miseprintln!(
+            "{:?}",
+            Dot::with_attr_getters(
+                &deps.graph,
+                &[
+                    petgraph::dot::Config::NodeNoLabel,
+                    petgraph::dot::Config::EdgeNoLabel
+                ],
+                &|_, _| String::new(),
+                &|_, nr| format!("label = \"{}\"", nr.1.name),
+            ),
+        );
+        Ok(())
+    }
+
+    fn err_no_task(&self, config: &Config, t: &str) -> eyre::Report {
+        let tasks = config.tasks();
+        let task_names = tasks.keys().sorted().map(style::ecyan).join(", ");
+        let t = style(&t).yellow().for_stderr();
+        eyre!("no task named `{t}` found. Available tasks: {task_names}")
+    }
+}
+
+static AFTER_LONG_HELP: &str = color_print::cstr!(
+    r#"<bold><underline>Examples:</underline></bold>
+  $ <bold>mise task deps</bold>
+  Shows dependencies for all tasks
+
+  $ <bold>mise task deps lint test check</bold>
+  Shows dependencies for the "lint", "test" and "check" tasks
+
+  $ <bold>mise task deps --dot</bold>
+  Shows dependencies in DOT format
+"#
+);
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_tasks_deps_tree() {
+        assert_cli_snapshot!("tasks", "deps", @r###"
+        configtask
+        filetask
+        ├── test
+        └── lint
+        lint
+        test
+        "###
+        );
+    }
+
+    #[test]
+    fn test_tasks_deps_tree_args() {
+        assert_cli_snapshot!("tasks", "deps", "filetask", "lint", "test", @r###"
+        filetask
+        ├── test
+        └── lint
+        lint
+        test
+        "###
+        );
+    }
+
+    #[test]
+    fn test_tasks_deps_dot() {
+        assert_cli_snapshot!("tasks", "deps", "--dot", @r###"
+        digraph {
+            0 [ label = "configtask"]
+            1 [ label = "filetask"]
+            2 [ label = "lint"]
+            3 [ label = "test"]
+            1 -> 2 [ ]
+            1 -> 3 [ ]
+        }
+        "###
+        );
+    }
+}
--- src/task.rs
+++ src/task.rs
@@ -1,9 +1,14 @@
+use petgraph::graph::DiGraph;
+use petgraph::prelude::*;
+use petgraph::{Direction, Graph};
+use std::borrow::Cow;
 use std::cmp::Ordering;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fmt;
 use std::fmt::{Display, Formatter};
 use std::hash::{Hash, Hasher};
 use std::path::PathBuf;
+use std::sync::mpsc;
 
 use console::truncate_str;
 use eyre::Result;
@@ -12,6 +17,7 @@ use itertools::Itertools;
 use crate::config::config_file::toml::TomlParser;
 use crate::config::Config;
 use crate::file;
+use crate::ui::tree::TreeItem;
 
 #[derive(Debug, Default, Clone, Eq, PartialEq)]
 pub struct Task {
@@ -118,7 +124,7 @@ impl Task {
     }
 
     pub fn resolve_depends<'a>(&self, config: &'a Config) -> Result<Vec<&'a Task>> {
-        let tasks = config.tasks();
+        let tasks = config.tasks_with_aliases();
         let depends = self
             .depends
             .iter()
@@ -178,5 +184,120 @@ impl Ord for Task {
 impl Hash for Task {
     fn hash<H: Hasher>(&self, state: &mut H) {
         self.name.hash(state);
+    }
+}
+
+#[derive(Debug)]
+pub struct Deps {
+    pub graph: DiGraph<Task, ()>,
+    sent: HashSet<String>,
+    tx: mpsc::Sender<Option<Task>>,
+}
+
+impl Deps {
+    pub fn new(config: &Config, tasks: Vec<Task>) -> Result<Self> {
+        let mut graph = DiGraph::new();
+        let mut indexes = HashMap::new();
+        let mut stack = vec![];
+        for t in tasks {
+            stack.push(t.clone());
+            indexes
+                .entry(t.name.clone())
+                .or_insert_with(|| graph.add_node(t));
+        }
+        while let Some(a) = stack.pop() {
+            let a_idx = *indexes
+                .entry(a.name.clone())
+                .or_insert_with(|| graph.add_node(a.clone()));
+            for b in a.resolve_depends(config)? {
+                let b_idx = *indexes
+                    .entry(b.name.clone())
+                    .or_insert_with(|| graph.add_node(b.clone()));
+                if !graph.contains_edge(a_idx, b_idx) {
+                    graph.add_edge(a_idx, b_idx, ());
+                }
+                stack.push(b.clone());
+            }
+        }
+        let (tx, _) = mpsc::channel();
+        let sent = HashSet::new();
+        Ok(Self { graph, tx, sent })
+    }
+
+    fn leaves(&self) -> Vec<Task> {
+        self.graph
+            .externals(Direction::Outgoing)
+            .map(|idx| self.graph[idx].clone())
+            .collect()
+    }
+
+    fn emit_leaves(&mut self) {
+        let leaves = self.leaves().into_iter().collect_vec();
+        for task in leaves {
+            if self.sent.contains(&task.name) {
+                continue;
+            }
+            self.sent.insert(task.name.clone());
+            self.tx.send(Some(task)).unwrap();
+        }
+        if self.graph.node_count() == 0 {
+            self.tx.send(None).unwrap();
+        }
+    }
+
+    pub fn subscribe(&mut self) -> mpsc::Receiver<Option<Task>> {
+        let (tx, rx) = mpsc::channel();
+        self.tx = tx;
+        self.emit_leaves();
+        rx
+    }
+
+    // #[requires(self.graph.node_count() > 0)]
+    // #[ensures(self.graph.node_count() == old(self.graph.node_count()) - 1)]
+    pub fn remove(&mut self, task: &Task) {
+        if let Some(idx) = self
+            .graph
+            .node_indices()
+            .find(|&idx| &self.graph[idx] == task)
+        {
+            self.graph.remove_node(idx);
+            self.emit_leaves();
+        }
+    }
+
+    pub fn all(&self) -> impl Iterator<Item = &Task> {
+        self.graph.node_indices().map(|idx| &self.graph[idx])
+    }
+
+    pub fn is_linear(&self) -> bool {
+        !self.graph.node_indices().any(|idx| {
+            self.graph
+                .neighbors_directed(idx, Direction::Outgoing)
+                .count()
+                > 1
+        })
+    }
+
+    // fn pop(&'a mut self) -> Option<&'a Task> {
+    //     if let Some(leaf) = self.leaves().first() {
+    //         self.remove(&leaf.clone())
+    //     } else {
+    //         None
+    //     }
+    // }
+}
+
+impl TreeItem for (&Graph<Task, ()>, NodeIndex) {
+    type Child = Self;
+
+    fn write_self(&self) {
+        if let Some(w) = self.0.node_weight(self.1) {
+            miseprint!("{}", w.name);
+        }
+    }
+
+    fn children(&self) -> Cow<[Self::Child]> {
+        let v: Vec<_> = self.0.neighbors(self.1).map(|i| (self.0, i)).collect();
+        Cow::from(v)
     }
 }
