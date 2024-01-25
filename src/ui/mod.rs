ba0cdb01b9523308554f99e03900aa44bd35452f
jdx <216188+jdx@users.noreply.github.com>
2024-01-25 17:43:48 -0600

    pass signals to tasks (#1527)
    * pass signals to tasks
    
    * [MegaLinter] Apply linters fixes
    
    ---------
    
    Co-authored-by: jdx <jdx@users.noreply.github.com>

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,26 +1,9 @@
-use std::process::exit;
-use std::sync::Once;
-
-use console::{user_attended_stderr, Term};
-
 pub use prompt::confirm;
 
+pub mod ctrlc;
 pub mod multi_progress_report;
 pub mod progress_report;
 pub mod prompt;
 pub mod style;
 pub mod table;
 pub mod tree;
-
-pub fn handle_ctrlc() {
-    static ONCE: Once = Once::new();
-    ONCE.call_once(|| {
-        if user_attended_stderr() {
-            let _ = ctrlc::set_handler(move || {
-                let _ = Term::stderr().show_cursor();
-                debug!("Ctrl-C pressed, exiting...");
-                exit(1);
-            });
-        }
-    });
-}
