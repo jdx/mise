8a9a284f1520500a361c1bc2f4db09648a49acd2
Jeff Dickey <216188+jdx@users.noreply.github.com>
2024-02-09 16:52:20 -0600

    bump msrv for clap compatibility

--- scripts/pre-release-hook.sh
+++ scripts/pre-release-hook.sh
@@ -3,8 +3,8 @@ set -euxo pipefail
 
 if [[ "${NO_UPDATE:-}" == "1" ]]; then
   echo "NO_UPDATE is set, skipping update"
-else
-  cargo update && git add Cargo.lock
+# else
+#   cargo update && git add Cargo.lock
 fi
 
 ./scripts/update-shorthand-repo.sh
