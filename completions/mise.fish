ce581ea7f1ca51ca35988b811c956bbe64671851
Jeff Dickey <216188+jdx@users.noreply.github.com>
2024-02-10 15:13:15 -0600

    chore: Release mise version 2024.2.11

--- completions/mise.fish
+++ completions/mise.fish
@@ -1,2 +1,8 @@
+# if "usage" is not installed show an error
+if ! command -v usage &> /dev/null
+    echo "Error: usage not found. This is required for completions to work in mise." >&2
+    return 1
+end
+
 set _usage_spec_mise (mise usage | string collect)
-complete -xc mise -a '(/Users/jdx/src/usage/target/debug/usage complete-word -s "$_usage_spec_mise" -- (commandline -cop) (commandline -t))'
+complete -xc mise -a '(usage complete-word -s "$_usage_spec_mise" -- (commandline -cop) (commandline -t))'
