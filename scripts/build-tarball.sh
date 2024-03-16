46d447969dee7275188ede719f52200361b4afbf
Jeff Dickey <216188+jdx@users.noreply.github.com>
2024-03-16 18:53:26 -0500

    style: run lint-fix which has changed slightly

--- scripts/build-tarball.sh
+++ scripts/build-tarball.sh
@@ -2,75 +2,75 @@
 set -euo pipefail
 
 error() {
-  echo "$@" >&2
-  exit 1
+	echo "$@" >&2
+	exit 1
 }
 
 NAME="$1"
 shift
 
 for arg in "$@"; do
-  if [ "${next_target:-}" = 1 ]; then
-    next_target=
-    TARGET="$arg"
-    continue
-  fi
-  case "$arg" in
-    --target)
-      next_target=1
-      ;;
-    *) ;;
+	if [ "${next_target:-}" = 1 ]; then
+		next_target=
+		TARGET="$arg"
+		continue
+	fi
+	case "$arg" in
+	--target)
+		next_target=1
+		;;
+	*) ;;
 
-  esac
+	esac
 done
 
 RUST_TRIPLE=${TARGET:-$(rustc -vV | grep ^host: | cut -d ' ' -f2)}
 #region os/arch
 get_os() {
-  case "$RUST_TRIPLE" in
-    *-apple-darwin*)
-      echo "macos"
-      ;;
-    *-linux-*)
-      echo "linux"
-      ;;
-    *)
-      error "unsupported OS: $RUST_TRIPLE"
-      ;;
-  esac
+	case "$RUST_TRIPLE" in
+	*-apple-darwin*)
+		echo "macos"
+		;;
+	*-linux-*)
+		echo "linux"
+		;;
+	*)
+		error "unsupported OS: $RUST_TRIPLE"
+		;;
+	esac
 }
 
 get_arch() {
-  case "$RUST_TRIPLE" in
-    aarch64-*)
-      echo "arm64"
-      ;;
-    arm-*)
-      echo "armv6"
-      ;;
-    armv7-*)
-      echo "armv7"
-      ;;
-    x86_64-*)
-      echo "x64"
-      ;;
-    universal2-*)
-      echo "universal"
-      ;;
-    *)
-      error "unsupported arch: $RUST_TRIPLE"
-      ;;
-  esac
+	case "$RUST_TRIPLE" in
+	aarch64-*)
+		echo "arm64"
+		;;
+	arm-*)
+		echo "armv6"
+		;;
+	armv7-*)
+		echo "armv7"
+		;;
+	x86_64-*)
+		echo "x64"
+		;;
+	universal2-*)
+		echo "universal"
+		;;
+	*)
+		error "unsupported arch: $RUST_TRIPLE"
+		;;
+	esac
 }
 get_suffix() {
-  case "$RUST_TRIPLE" in
-    *-musl | *-musleabi | *-musleabihf)
-      echo "-musl"
-      ;;
-    *)
-      echo ""
-      ;;
-  esac
+	case "$RUST_TRIPLE" in
+	*-musl | *-musleabi | *-musleabihf)
+		echo "-musl"
+		;;
+	*)
+		echo ""
+		;;
+	esac
 }
 #endregion
 
@@ -79,11 +79,11 @@ VERSION=$(./scripts/get-version.sh)
 BASENAME=$NAME-$VERSION-$(get_os)-$(get_arch)$(get_suffix)
 
 if command -v cross >/dev/null; then
-  cross build "$@"
+	cross build "$@"
 elif command -v zig >/dev/null; then
-  cargo zigbuild "$@"
+	cargo zigbuild "$@"
 else
-  cargo build "$@"
+	cargo build "$@"
 fi
 mkdir -p dist/mise/bin
 mkdir -p dist/mise/man/man1
@@ -99,8 +99,8 @@ tar -cJf "$BASENAME.tar.xz" mise
 tar -czf "$BASENAME.tar.gz" mise
 
 if [ -f ~/.zipsign/mise.priv ]; then
-  zipsign sign tar "$BASENAME.tar.gz" ~/.zipsign/mise.priv
-  zipsign verify tar "$BASENAME.tar.gz" ../zipsign.pub
+	zipsign sign tar "$BASENAME.tar.gz" ~/.zipsign/mise.priv
+	zipsign verify tar "$BASENAME.tar.gz" ../zipsign.pub
 fi
 
 ls -oh "$BASENAME.tar.xz"
