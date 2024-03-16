ce533eea42fef76cac7f2071df513a39fcc93081
Jeff Dickey <216188+jdx@users.noreply.github.com>
2024-03-16 18:56:04 -0500

    style: apply editorconfig changes

--- packaging/deb/generate-release.sh
+++ packaging/deb/generate-release.sh
@@ -6,16 +6,16 @@ set -euo pipefail
 # shellcheck disable=SC2185
 
 do_hash() {
-  HASH_NAME=$1
-  HASH_CMD=$2
-  echo "${HASH_NAME}:"
-  for f in $(find -type f); do
-    f=$(echo $f | cut -c3-) # remove ./ prefix
-    if [ "$f" = "Release" ]; then
-      continue
-    fi
-    echo " $(${HASH_CMD} ${f} | cut -d" " -f1) $(wc -c $f)"
-  done
+	HASH_NAME=$1
+	HASH_CMD=$2
+	echo "${HASH_NAME}:"
+	for f in $(find -type f); do
+		f=$(echo $f | cut -c3-) # remove ./ prefix
+		if [ "$f" = "Release" ]; then
+			continue
+		fi
+		echo " $(${HASH_CMD} ${f} | cut -d" " -f1) $(wc -c $f)"
+	done
 }
 
 cat <<EOF
