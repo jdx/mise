3c9920eb8565fe0744e207994009432ba298896a
Roland Schaer <roele33@hotmail.com>
2024-04-18 09:37:14 +0200

    refactor: write ForgeMeta to JSON instead of TOML

--- src/forge/forge_meta.rs
+++ src/forge/forge_meta.rs
@@ -8,15 +8,16 @@ use super::ForgeType;
 pub struct ForgeMeta {
     pub id: String,
     pub name: String,
+    pub forge_type: String,
 }
 
-const FORGE_META_FILENAME: &str = ".mise.forge.toml";
+const FORGE_META_FILENAME: &str = ".mise.forge.json";
 
 impl ForgeMeta {
     pub fn read(dirname: &str) -> ForgeMeta {
         let meta_path = &dirs::INSTALLS.join(dirname).join(FORGE_META_FILENAME);
-        let toml = file::read_to_string(meta_path).unwrap_or_default();
-        toml::from_str(&toml).unwrap_or(Self::default_meta(dirname))
+        let json = file::read_to_string(meta_path).unwrap_or_default();
+        serde_json::from_str(&json).unwrap_or(Self::default_meta(dirname))
     }
 
     pub fn write(fa: &ForgeArg) -> eyre::Result<()> {
@@ -26,29 +27,37 @@ impl ForgeMeta {
         let meta = ForgeMeta {
             id: fa.id.clone(),
             name: fa.name.clone(),
+            forge_type: fa.forge_type.as_ref().to_string(),
         };
-        let toml = toml::to_string(&meta).expect("Could not encode TOML value");
+
+        let json = serde_json::to_string(&meta).expect("Could not encode JSON value");
         let meta_path = fa.installs_path.join(FORGE_META_FILENAME);
-        file::write(meta_path, toml)?;
+        file::write(meta_path, json)?;
         Ok(())
     }
 
-    // Returns a ForgeMeta with id and name derived from the dirname for backends without a .mise.forge.toml file
+    // Returns a ForgeMeta derived from the dirname for forges without a meta file
     fn default_meta(dirname: &str) -> ForgeMeta {
         let id = dirname.replacen('-', ":", 1);
         match id.split_once(':') {
             Some((forge_type, name)) => {
                 let name = Self::name_for_type(name.to_string(), forge_type);
                 let id = format!("{}:{}", forge_type, name);
-                ForgeMeta { id, name }
+                ForgeMeta {
+                    id,
+                    name,
+                    forge_type: forge_type.to_string(),
+                }
             }
             None => ForgeMeta {
                 id: id.to_string(),
                 name: id.to_string(),
+                forge_type: ForgeType::Asdf.as_ref().to_string(),
             },
         }
     }
 
+    // TODO: remove this when backends come out of experimental
     fn name_for_type(name: String, forge_type: &str) -> String {
         match forge_type {
             "go" => name.replace('-', "/"),
