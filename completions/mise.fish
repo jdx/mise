5b05362d57a19619ba8b4fc142f8441c1ce24ad0
Onni Hakala <onni@flaky.build>
2024-04-25 12:30:38 +0200

    Use usage through mise automatically if it's not installed in fish shell

--- completions/mise.fish
+++ completions/mise.fish
@@ -1,9 +1,12 @@
 # if "usage" is not installed show an error
 if ! command -v usage &> /dev/null
-    echo >&2
-    echo "Error: usage CLI not found. This is required for completions to work in mise." >&2
-    echo "See https://usage.jdx.dev for more information." >&2
-    return 1
+    # Try to enable usage through mise
+    if ! mise use -g usage &> /dev/null
+        echo >&2
+        echo "Error: usage CLI not found. This is required for completions to work in mise." >&2
+        echo "See https://usage.jdx.dev for more information." >&2
+        return 1
+    end
 end
 
 set _usage_spec_mise (mise usage | string collect)
