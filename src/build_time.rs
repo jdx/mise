5cb4cb2f13320da6e7e4e1684e1e4e7cf151bfe8
jdx <216188+jdx@users.noreply.github.com>
2024-04-26 21:13:54 -0500

    chore: make git2 an optional build dependency (#1960)

--- src/build_time.rs
+++ src/build_time.rs
@@ -5,5 +5,15 @@ pub mod built_info {
     include!(concat!(env!("OUT_DIR"), "/built.rs"));
 }
 
+#[cfg(feature = "git2")]
+pub fn git_sha() -> &'static Option<&'static str> {
+    &built_info::GIT_COMMIT_HASH_SHORT
+}
+
+#[cfg(not(feature = "git2"))]
+pub fn git_sha() -> &'static Option<&'static str> {
+    &None
+}
+
 pub static BUILD_TIME: Lazy<DateTime<FixedOffset>> =
     Lazy::new(|| DateTime::parse_from_rfc2822(built_info::BUILT_TIME_UTC).unwrap());
