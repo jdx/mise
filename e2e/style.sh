afbc7f0caa43fabdcb9a5db72f7d8dcad1857a4e
Adirelle <adirelle@gmail.com>
2024-05-10 21:07:14 +0200

    test: tweak group and notice output messages.

--- e2e/style.sh
+++ e2e/style.sh
@@ -5,7 +5,7 @@ if [[ -n ${GITHUB_ACTION:-} ]]; then
   # Use ANSI green color for the "ok" message so groups with no errors are kept collapsed
   _STYLE_OK=$'\e[92m'
   _STYLE_ERR='::error::'
-  _STYLE_NOTICE='::warning::'
+  _STYLE_WARN='::warning::'
   _STYLE_RESET=$'\e[0m'
   _GROUP_START='::group::'
   _GROUP_END='::endgroup::'
@@ -13,7 +13,7 @@ elif [[ -t 2 ]]; then
   # Use ANSI coloring in terminal
   _STYLE_OK=$'\e[92m'       # green
   _STYLE_ERR=$'\e[91m'      # red
-  _STYLE_NOTICE=$'\e[93m'   # yellow
+  _STYLE_WARN=$'\e[93m'     # yellow
   _STYLE_RESET=$'\e[0m'     # full reset
   _GROUP_START=$'\e[1m>>> ' # bold
   _GROUP_END=
@@ -21,8 +21,8 @@ else
   # No styling
   _STYLE_OK='SUCCESS: '
   _STYLE_ERR='ERROR: '
-  _STYLE_NOTICE='NOTICE: '
-  _STYLE_RESET=''
+  _STYLE_WARN='NOTICE: '
+  _STYLE_RESET=
   _GROUP_START='>>> '
   _GROUP_END=
 fi
@@ -35,8 +35,8 @@ err() {
   echo "${_STYLE_ERR}$*${_STYLE_RESET}" >&2
 }
 
-notice() {
-  echo "${_STYLE_NOTICE}$*${_STYLE_RESET}" >&2
+warn() {
+  echo "${_STYLE_WARN}$*${_STYLE_RESET}" >&2
 }
 
 as_group() {
@@ -44,6 +44,6 @@ as_group() {
   echo "${_GROUP_START}$1${_STYLE_RESET}" >&2
   shift
   "$*" || status=$?
-  echo "${_GROUP_END}" >&2
+  echo "${_GROUP_END}${_STYLE_RESET}" >&2
   return "$status"
 }
