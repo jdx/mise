3e7571cc33bb456fb7246c0735ce26bf9f825e95
Roland Schaer <roele33@hotmail.com>
2024-05-16 12:54:31 +0200

    feat: confirm all plugins (#2126)

--- src/ui/prompt.rs
+++ src/ui/prompt.rs
@@ -1,11 +1,13 @@
 use std::sync::Mutex;
 
-use demand::Confirm;
+use demand::{Confirm, Dialog, DialogButton};
 
 use crate::env;
 use crate::ui::ctrlc;
 
 static MUTEX: Mutex<()> = Mutex::new(());
+
+static SKIP_PROMPT: Mutex<bool> = Mutex::new(false);
 
 pub fn confirm<S: Into<String>>(message: S) -> eyre::Result<bool> {
     let _lock = MUTEX.lock().unwrap(); // Prevent multiple prompts at once
@@ -15,5 +17,39 @@ pub fn confirm<S: Into<String>>(message: S) -> eyre::Result<bool> {
         return Ok(false);
     }
     let result = Confirm::new(message).run()?;
+    Ok(result)
+}
+
+pub fn confirm_with_all<S: Into<String>>(message: S) -> eyre::Result<bool> {
+    let _lock = MUTEX.lock().unwrap(); // Prevent multiple prompts at once
+    let _ctrlc = ctrlc::handle_ctrlc()?;
+
+    if !console::user_attended_stderr() || env::__USAGE.is_some() {
+        return Ok(false);
+    }
+
+    let mut skip_prompt = SKIP_PROMPT.lock().unwrap();
+    if *skip_prompt {
+        return Ok(true);
+    }
+
+    let answer = Dialog::new(message)
+        .buttons(vec![
+            DialogButton::new("Yes"),
+            DialogButton::new("No"),
+            DialogButton::new("All"),
+        ])
+        .selected_button(1)
+        .run()?;
+
+    let result = match answer.as_str() {
+        "Yes" => true,
+        "No" => false,
+        "All" => {
+            *skip_prompt = true;
+            true
+        }
+        _ => false,
+    };
     Ok(result)
 }
