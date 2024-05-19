09daa7075d70769f6e00b465d705cb9366b9f18e
jdx <216188+jdx@users.noreply.github.com>
2024-05-19 16:41:50 -0500

    feat: added registry command (#2147)

--- scripts/render-registry.js
+++ scripts/render-registry.js
@@ -3,19 +3,34 @@
 const {execSync} = require("node:child_process");
 const fs = require('node:fs');
 
-const stdout = execSync("mise plugins --all --urls");
+const stdout = execSync("mise registry", {encoding: "utf-8"});
 // Regular expression to match plugin name and repository URL
-const regex = /^([\w-]+)\s+(https?:\/\/\S+)\s*$/gm;
+// e.g.: zprint asdf:carlduevel/asdf-zprint
+const regex = /^(.+?) +(.+?):(.+?) *$/gm;
 
 let match;
-let output = "---\neditLink: false\n---\n";
+let output = ["---\neditLink: false\n---"];
 
-output +=
-  "| Tool | Repository URL |\n| ----------- | --------------- |";
+output.push("| Short | Full |\n| ----------- | --------------- |");
 while ((match = regex.exec(stdout)) !== null) {
-  const repoUrl = match[2].replace(/\.git$/, "");
-  output += `\n| ${match[1]} | <${repoUrl}> |`;
+    if (match[2] === "asdf") {
+        let repoUrl = match[3].replace(/\.git$/, "");
+        if (!repoUrl.startsWith("http")) {
+            repoUrl = `https://github.com/${repoUrl}`;
+        }
+        output.push(`| ${match[1]} | [${match[2]}:${match[3]}](${repoUrl}) |`);
+    } else if (match[2] === 'core') {
+        output.push(`| ${match[1]} | [${match[2]}:${match[3]}](https://mise.jdx.dev/lang/${match[2]}.html) |`);
+    } else if (match[2] === 'cargo') {
+        output.push(`| ${match[1]} | [${match[2]}:${match[3]}](https://crates.io/crates/${match[3]}) |`);
+    } else if (match[2] === 'npm') {
+        output.push(`| ${match[1]} | [${match[2]}:${match[3]}](https://www.npmjs.com/package/${match[3]}) |`);
+    } else if (match[2] === 'pipx') {
+        output.push(`| ${match[1]} | [${match[2]}:${match[3]}](https://pypi.org/project/${match[3]}) |`);
+    } else {
+        output.push(`| ${match[1]} | ${match[2]}:${match[3]} |`);
+    }
 }
-output += "\n";
+output.push('');
 
-fs.writeFileSync("docs/registry.md", output);
+fs.writeFileSync("docs/registry.md", output.join('\n'));
---
+++ src/cli/registry.rs
@@ -0,0 +1,78 @@
+use std::collections::BTreeMap;
+
+use eyre::Result;
+use tabled::{Table, Tabled};
+
+use crate::config::{settings, Config};
+use crate::registry::REGISTRY;
+use crate::ui::table;
+
+/// [experimental] List available tools
+#[derive(Debug, clap::Args)]
+#[clap(after_long_help = AFTER_LONG_HELP, verbatim_doc_comment)]
+pub struct Registry {}
+
+impl Registry {
+    pub fn run(self) -> Result<()> {
+        settings::ensure_experimental("registry")?;
+        let mut tools = BTreeMap::new();
+
+        for (plugin, url) in Config::get().get_shorthands() {
+            let re = regex!(r#"^https://github.com/(.+?/.+?)(.git)?$"#);
+            let full = if let Some(caps) = re.captures(url) {
+                format!("asdf:{}", &caps[1])
+            } else {
+                format!("asdf:{}", url)
+            };
+            tools.insert(plugin.to_string(), full);
+        }
+
+        for (short, full) in REGISTRY.iter() {
+            tools.insert(short.to_string(), full.to_string());
+        }
+
+        let data = tools.into_iter().map(|x| x.into()).collect::<Vec<Row>>();
+        let mut table = Table::new(data);
+        table::default_style(&mut table, false);
+        miseprintln!("{table}");
+        Ok(())
+    }
+}
+
+#[derive(Tabled, Eq, PartialEq, Ord, PartialOrd)]
+#[tabled(rename_all = "PascalCase")]
+struct Row {
+    short: String,
+    full: String,
+}
+
+impl From<(String, String)> for Row {
+    fn from((short, full): (String, String)) -> Self {
+        Self { short, full }
+    }
+}
+
+static AFTER_LONG_HELP: &str = color_print::cstr!(
+    r#"<bold><underline>Examples:</underline></bold>
+
+    $ <bold>mise registry</bold>
+    node    core:node
+    poetry  asdf:mise-plugins/mise-poetry
+    ubi     cargo:ubi
+"#
+);
+
+#[cfg(test)]
+mod tests {
+    use test_log::test;
+
+    use crate::cli::tests::grep;
+    use crate::test::reset;
+
+    #[test]
+    fn test_registry() {
+        reset();
+        let out = assert_cli!("registry");
+        assert_snapshot!(grep(out, "node"), @"node                         core:node");
+    }
+}
