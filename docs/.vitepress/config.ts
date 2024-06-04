ae797c9b455a291b0cef112b6dd1cead6c6c8aff
Kattouf <vasiliy.kattouf@gmail.com>
2024-06-04 17:18:54 +0700

    add item to site sidebar

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -50,6 +50,7 @@ export default defineConfig({
               { text: 'go', link: '/dev-tools/backends/go' },
               { text: 'npm', link: '/dev-tools/backends/npm' },
               { text: 'pipx', link: '/dev-tools/backends/pipx' },
+              { text: 'spm', link: '/dev-tools/backends/spm' },
               { text: 'ubi', link: '/dev-tools/backends/ubi' },
             ]
           }
