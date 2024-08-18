16a08fc0fa00fc8f9751f7a25cc4f5f5fc87b94d
Jeff Dickey <216188+jdx@users.noreply.github.com>
2024-08-18 14:22:09 -0500

    fix: hide non-working core plugins on windows

--- build.rs
+++ build.rs
@@ -1,9 +1,5 @@
 fn main() {
     cfg_aliases::cfg_aliases! {
-        linux: { target_os = "linux" },
-        macos: { target_os = "macos" },
-        windows: { target_os = "windows" },
-
         vfox: { any(feature = "vfox", target_os = "windows") },
         asdf: { any(feature = "asdf", not(target_os = "windows")) },
     }
