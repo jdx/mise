f72d5f21581a52ae1b6eb98f6a4e961de38c7771
Roland Schaer <roele33@hotmail.com>
2024-10-07 06:33:10 +0200

    feat: task shell (#2709)

--- src/cli/tasks/ls.rs
+++ src/cli/tasks/ls.rs
@@ -194,10 +194,12 @@ mod tests {
     fn test_task_ls() {
         reset();
         assert_cli_snapshot!("t", "--no-headers", @r#"
-        configtask                               ~/config/config.toml
-        filetask    This is a test build script  ~/cwd/.mise/tasks/filetask
-        lint                                     ~/config/config.toml
-        test                                     ~/config/config.toml
+        configtask                                  ~/config/config.toml
+        filetask       This is a test build script  ~/cwd/.mise/tasks/filetask
+        lint                                        ~/config/config.toml
+        shell                                       ~/config/config.toml
+        shell invalid                               ~/config/config.toml
+        test                                        ~/config/config.toml
         "#);
     }
 
