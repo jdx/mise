5afbab0a8ce41057653c3d5bea054a321e1d19fd
Jeff Dickey <216188+jdx@users.noreply.github.com>
2024-10-13 16:04:55 -0500

    fix: enable tokio timing for ubi

--- src/backend/ubi.rs
+++ src/backend/ubi.rs
@@ -79,6 +79,7 @@ impl Backend for UbiBackend {
 
         let rt = tokio::runtime::Builder::new_current_thread()
             .enable_io()
+            .enable_time()
             .build()?;
         rt.block_on(u.install_binary()).map_err(|e| eyre!(e))
     }
