06053a39ed74613f5f889e5be33cb643f799db46
jdx <216188+jdx@users.noreply.github.com>
2024-10-13 17:19:33 -0500

    fix: automatically prefix ubi installs with "v" if not found (#2747)

--- src/github.rs
+++ src/github.rs
@@ -14,3 +14,11 @@ pub fn list_releases(repo: &str) -> eyre::Result<Vec<GithubRelease>> {
     let url = format!("https://api.github.com/repos/{}/releases", repo);
     crate::http::HTTP_FETCH.json(url)
 }
+
+pub fn get_release(repo: &str, tag: &str) -> eyre::Result<GithubRelease> {
+    let url = format!(
+        "https://api.github.com/repos/{}/releases/tags/{}",
+        repo, tag
+    );
+    crate::http::HTTP_FETCH.json(url)
+}
