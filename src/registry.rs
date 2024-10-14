628fa2274129715937fd880b43321f3ccc2ea28f
Jeff Dickey <216188+jdx@users.noreply.github.com>
2024-10-14 02:40:02 -0500

    fix: use asdf by default unless on windows

--- src/registry.rs
+++ src/registry.rs
@@ -14,6 +14,7 @@ include!(concat!(env!("OUT_DIR"), "/registry.rs"));
 pub static REGISTRY: Lazy<BTreeMap<&str, String>> = Lazy::new(|| {
     let backend_types = vec!["ubi", "vfox", "asdf", "cargo", "go", "npm", "pipx", "spm"]
         .into_iter()
+        .filter(|b| cfg!(unix) || *b != "asdf")
         .filter(|b| !SETTINGS.disable_backends.contains(&b.to_string()))
         .collect::<HashSet<_>>();
 
