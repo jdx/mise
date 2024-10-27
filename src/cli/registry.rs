c52be46962f05726bfb01e945160a84b3aca2e6e
jdx <216188+jdx@users.noreply.github.com>
2024-10-27 08:15:11 -0500

    feat: accept argument for `mise registry` (#2841)

--- src/cli/registry.rs
+++ src/cli/registry.rs
@@ -1,44 +1,37 @@
-use std::collections::BTreeMap;
-
-use crate::config::{settings, Config};
 use crate::registry::REGISTRY;
 use crate::ui::table;
-use eyre::Result;
+use eyre::{bail, Result};
 use tabled::{Table, Tabled};
-use xx::regex;
 
-/// [experimental] List available tools to install
+/// List available tools to install
 ///
 /// This command lists the tools available in the registry as shorthand names.
 ///
 /// For example, `poetry` is shorthand for `asdf:mise-plugins/mise-poetry`.
 #[derive(Debug, clap::Args)]
 #[clap(after_long_help = AFTER_LONG_HELP, verbatim_doc_comment)]
-pub struct Registry {}
+pub struct Registry {
+    /// Show only the specified tool's full name
+    name: Option<String>,
+}
 
 impl Registry {
     pub fn run(self) -> Result<()> {
-        settings::ensure_experimental("registry")?;
-        let mut tools = BTreeMap::new();
-
-        let re = regex!(r#"^https://github.com/(.+?/.+?)(.git)?$"#);
-        for (plugin, url) in Config::get().get_shorthands() {
-            let full = if let Some(caps) = re.captures(url) {
-                format!("asdf:{}", &caps[1])
+        if let Some(name) = &self.name {
+            if let Some(full) = REGISTRY.get(name.as_str()) {
+                miseprintln!("{full}");
             } else {
-                format!("asdf:{}", url)
-            };
-            tools.insert(plugin.to_string(), full);
+                bail!("tool not found in registry: {name}");
+            }
+        } else {
+            let data = REGISTRY
+                .iter()
+                .map(|(short, full)| (short.to_string(), full.to_string()).into())
+                .collect::<Vec<Row>>();
+            let mut table = Table::new(data);
+            table::default_style(&mut table, false);
+            miseprintln!("{table}");
         }
-
-        for (short, full) in REGISTRY.iter() {
-            tools.insert(short.to_string(), full.to_string());
-        }
-
-        let data = tools.into_iter().map(|x| x.into()).collect::<Vec<Row>>();
-        let mut table = Table::new(data);
-        table::default_style(&mut table, false);
-        miseprintln!("{table}");
         Ok(())
     }
 }
@@ -63,6 +56,9 @@ static AFTER_LONG_HELP: &str = color_print::cstr!(
     node    core:node
     poetry  asdf:mise-plugins/mise-poetry
     ubi     cargo:ubi-cli
+
+    $ <bold>mise registry poetry</bold>
+    asdf:mise-plugins/mise-poetry
 "#
 );
 
