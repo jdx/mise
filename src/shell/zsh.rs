fe67426bb92d0f49250ef36e8d67c7d2aa98bcae
Alex Efros <powerman@powerman.name>
2024-10-30 08:54:46 +0200

    fix(shell/zsh.rs): deactivate _mise_hook_cmd

--- src/shell/zsh.rs
+++ src/shell/zsh.rs
@@ -89,6 +89,7 @@ impl Shell for Zsh {
         formatdoc! {r#"
         precmd_functions=( ${{precmd_functions:#_mise_hook}} )
         chpwd_functions=( ${{chpwd_functions:#_mise_hook}} )
+        unset -f _mise_hook_cmd
         unset -f _mise_hook
         unset -f mise
         unset MISE_SHELL
