eee2b5af9064fc2b27a694f12bdf126b82000e9a
jdx <216188+jdx@users.noreply.github.com>
2024-11-09 09:19:21 -0600

    Update task.Tests.ps1

--- e2e-win/task.Tests.ps1
+++ e2e-win/task.Tests.ps1
@@ -1,6 +1,6 @@
 
 Describe 'task' {
     It 'executes a task' {
-        mise run filetask.bat | Should -BeLike '*mytask'
+        mise run filetask.bat | Select -Last 1 | Should -Be 'mytask'
     }
 }
