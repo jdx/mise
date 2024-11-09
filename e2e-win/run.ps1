70ad636be5a1245311cf116fafa11d78139cc2b4
jdx <216188+jdx@users.noreply.github.com>
2024-11-09 09:58:44 -0600

    test: added win-e2e task test (#2972)
    * test: added win-e2e task test
    
    * Update task.Tests.ps1
    
    * Update task.Tests.ps1

--- e2e-win/run.ps1
+++ e2e-win/run.ps1
@@ -1,7 +1,15 @@
+param(
+    [string]$TestName
+)
+
 $config = New-PesterConfiguration
 $config.Run.Path = $PSScriptRoot
 $config.Run.Exit = $true
 $config.TestResult.Enabled = $true
+
+if ($TestName) {
+    $config.Filter.FullName = $TestName
+}
 
 $env:MISE_DEBUG = "1"
 
