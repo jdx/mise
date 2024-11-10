ac781207c7f5748c27cda8d9342a9b5c5cd868ff
mise-en-dev <release@mise.jdx.dev>
2024-11-10 08:41:20 -0600

    chore: release 2024.11.6 (#2975)

--- completions/mise.bash
+++ completions/mise.bash
@@ -6,11 +6,11 @@ _mise() {
         return 1
     fi
 
-    if [[ -z ${_usage_spec_mise_2024_11_5:-} ]]; then
-        _usage_spec_mise_2024_11_5="$(mise usage)"
+    if [[ -z ${_usage_spec_mise_2024_11_6:-} ]]; then
+        _usage_spec_mise_2024_11_6="$(mise usage)"
     fi
 
-    COMPREPLY=( $(usage complete-word --shell bash -s "${_usage_spec_mise_2024_11_5}" --cword="$COMP_CWORD" -- "${COMP_WORDS[@]}" ) )
+    COMPREPLY=( $(usage complete-word --shell bash -s "${_usage_spec_mise_2024_11_6}" --cword="$COMP_CWORD" -- "${COMP_WORDS[@]}" ) )
     if [[ $? -ne 0 ]]; then
         unset COMPREPLY
     fi
--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 
 rustPlatform.buildRustPackage {
   pname = "mise";
-  version = "2024.11.5";
+  version = "2024.11.6";
 
   src = lib.cleanSource ./.;
 
