d5dde8fa8731a0048e68137589982e16b605e648
mise-en-dev <release@mise.jdx.dev>
2024-11-11 21:01:02 -0600

    chore: release 2024.11.8 (#2998)

--- completions/mise.fish
+++ completions/mise.fish
@@ -6,7 +6,7 @@ if ! command -v usage &> /dev/null
     return 1
 end
 
-if ! set -q _usage_spec_mise_2024_11_7
-  set -U _usage_spec_mise_2024_11_7 (mise usage | string collect)
+if ! set -q _usage_spec_mise_2024_11_8
+  set -U _usage_spec_mise_2024_11_8 (mise usage | string collect)
 end
-complete -xc mise -a '(usage complete-word --shell fish -s "$_usage_spec_mise_2024_11_7" -- (commandline -cop) (commandline -t))'
+complete -xc mise -a '(usage complete-word --shell fish -s "$_usage_spec_mise_2024_11_8" -- (commandline -cop) (commandline -t))'
