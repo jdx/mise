6e6c0997e3c7e9d8af88234d4b9fd87fd2b5e5e7
mise-en-dev <release@mise.jdx.dev>
2024-11-21 16:17:32 -0600

    chore: release 2024.11.23 (#3126)

--- completions/mise.bash
+++ completions/mise.bash
@@ -6,11 +6,11 @@ _mise() {
         return 1
     fi
 
-    if [[ -z ${_usage_spec_mise_2024_11_22:-} ]]; then
-        _usage_spec_mise_2024_11_22="$(mise usage)"
+    if [[ -z ${_usage_spec_mise_2024_11_23:-} ]]; then
+        _usage_spec_mise_2024_11_23="$(mise usage)"
     fi
 
-    COMPREPLY=( $(usage complete-word --shell bash -s "${_usage_spec_mise_2024_11_22}" --cword="$COMP_CWORD" -- "${COMP_WORDS[@]}" ) )
+    COMPREPLY=( $(usage complete-word --shell bash -s "${_usage_spec_mise_2024_11_23}" --cword="$COMP_CWORD" -- "${COMP_WORDS[@]}" ) )
     if [[ $? -ne 0 ]]; then
         unset COMPREPLY
     fi
