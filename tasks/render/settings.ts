eacebf07c8c583c9a0dd9628cddbc6199b9b07a9
jdx <216188+jdx@users.noreply.github.com>
2024-11-22 09:01:33 -0600

    chore: simplify render:settings task (#3142)
    * chore: simplify render:settings task
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- tasks/render/settings.ts
+++ tasks/render/settings.ts
@@ -1,14 +1,10 @@
 import * as fs from "node:fs";
 import * as child_process from "node:child_process";
 import * as toml from "toml";
-import * as Handlebars from "handlebars";
-import { match, P } from "ts-pattern";
-
-const doc = toml.parse(fs.readFileSync("settings.toml", "utf-8"));
-const settings = {};
+import { match } from "ts-pattern";
 
 type Element = {
-  default: string;
+  default: string | number | boolean;
   description: string;
   deprecated: boolean;
   type: string;
@@ -18,12 +14,26 @@ type Element = {
   };
 };
 
-function buildElement(key, props): Element {
-  let type = props.type;
+type Props = {
+  type: string;
+  default: string | number | boolean;
+  description: string;
+  deprecated: boolean;
+  enum?: [string][];
+};
+
+function buildElement(key: string, props: Props): Element {
+  let { type } = props;
+  if (!type) {
+    throw new Error("Type property must be a string");
+  }
+
   if (type.startsWith("Option<")) {
     type = type.slice(7, -1);
   }
-  type = type.replaceAll("PathBuf", "String");
+
+  type = type.replace(/PathBuf/g, "Path").replaceAll("PathBuf", "String");
+
   type = match(type)
     .with("String", () => "string")
     .with("Path", () => "string")
@@ -36,27 +46,35 @@ function buildElement(key, props): Element {
     .otherwise(() => {
       throw new Error(`Unknown type: ${type}`);
     });
+
   if (!props.description) {
     console.error(`Missing description for ${key}`);
     process.exit(1);
   }
+
   const ele: Element = {
     default: props.default,
     description: props.description,
     deprecated: props.deprecated,
     type,
   };
+
   if (props.enum) {
     ele.enum = props.enum.map((e) => e[0]);
   }
+
   if (type === "string[]") {
     ele.type = "array";
     ele.items = {
       type: "string",
     };
   }
+
   return ele;
 }
+
+const doc = toml.parse(fs.readFileSync("settings.toml", "utf-8"));
+const settings: any = {};
 
 for (const key in doc) {
   const props = doc[key];
@@ -77,15 +95,9 @@ for (const key in doc) {
   }
 }
 
-const schema_tmpl = Handlebars.compile(
-  fs.readFileSync("schema/mise.json.hbs", "utf-8"),
-);
-fs.writeFileSync(
-  "schema/mise.json.tmp",
-  schema_tmpl({
-    settings_json: new Handlebars.SafeString(JSON.stringify(settings, null, 2)),
-  }),
-);
+const schema = JSON.parse(fs.readFileSync("schema/mise.json", "utf-8"));
+schema["$defs"].settings.properties = settings;
+fs.writeFileSync("schema/mise.json.tmp", JSON.stringify(schema, null, 2));
 
 child_process.execSync("jq . < schema/mise.json.tmp > schema/mise.json");
 child_process.execSync("prettier --write schema/mise.json");
