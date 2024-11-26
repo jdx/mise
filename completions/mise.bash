d07c118c80e36796cd92f215a7d9d9a5b35caa82
mise-en-dev <release@mise.jdx.dev>
2024-11-26 20:07:02 +0000

    chore: release 2024.11.30

--- completions/mise.bash
+++ completions/mise.bash
@@ -6,12 +6,12 @@ _mise() {
         return 1
     fi
 
-    if [[ -z ${_usage_spec_mise_2024_11_29:-} ]]; then
-        _usage_spec_mise_2024_11_29="$(mise usage)"
+    if [[ -z ${_usage_spec_mise_2024_11_30:-} ]]; then
+        _usage_spec_mise_2024_11_30="$(mise usage)"
     fi
 
     # shellcheck disable=SC2207
-    COMPREPLY=( $(usage complete-word --shell bash -s "${_usage_spec_mise_2024_11_29}" --cword="$COMP_CWORD" -- "${COMP_WORDS[@]}" ) )
+    COMPREPLY=( $(usage complete-word --shell bash -s "${_usage_spec_mise_2024_11_30}" --cword="$COMP_CWORD" -- "${COMP_WORDS[@]}" ) )
     if [[ $? -ne 0 ]]; then
         unset COMPREPLY
     fi
