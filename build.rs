3c330241653716e6e772f09e5cd5da37f0224ecf
jdx <216188+jdx@users.noreply.github.com>
2024-12-05 17:55:19 -0600

    fix: .terraform-version
    Fixes #3311
    Fixes #2702

--- build.rs
+++ build.rs
@@ -90,8 +90,19 @@ fn codegen_registry() {
                 depends
             })
             .unwrap_or_default();
+        let idiomatic_files = info
+            .get("idiomatic_files")
+            .map(|idiomatic_files| {
+                idiomatic_files
+                    .as_array()
+                    .unwrap()
+                    .iter()
+                    .map(|f| f.as_str().unwrap().to_string())
+                    .collect::<Vec<_>>()
+            })
+            .unwrap_or_default();
         let rt = format!(
-            r#"RegistryTool{{short: "{short}", backends: vec!["{backends}"], aliases: &[{aliases}], test: &{test}, os: &[{os}], depends: &[{depends}]}}"#,
+            r#"RegistryTool{{short: "{short}", backends: vec!["{backends}"], aliases: &[{aliases}], test: &{test}, os: &[{os}], depends: &[{depends}], idiomatic_files: &[{idiomatic_files}]}}"#,
             backends = fulls.join("\", \""),
             aliases = aliases
                 .iter()
@@ -109,6 +120,11 @@ fn codegen_registry() {
             depends = depends
                 .iter()
                 .map(|d| format!("\"{d}\""))
+                .collect::<Vec<_>>()
+                .join(", "),
+            idiomatic_files = idiomatic_files
+                .iter()
+                .map(|f| format!("\"{f}\""))
                 .collect::<Vec<_>>()
                 .join(", "),
         );
