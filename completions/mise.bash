c36419844d80c51ef34870fb3fb3a62be375a411
mise-en-dev <release@mise.jdx.dev>
2024-12-06 06:35:09 -0600

    chore: release 2024.12.1 (#3357)

--- completions/mise.bash
+++ completions/mise.bash
@@ -6,12 +6,12 @@ _mise() {
         return 1
     fi
 
-    if [[ -z ${_usage_spec_mise_2024_12_0:-} ]]; then
-        _usage_spec_mise_2024_12_0="$(mise usage)"
+    if [[ -z ${_usage_spec_mise_2024_12_1:-} ]]; then
+        _usage_spec_mise_2024_12_1="$(mise usage)"
     fi
 
     # shellcheck disable=SC2207
-    COMPREPLY=( $(usage complete-word --shell bash -s "${_usage_spec_mise_2024_12_0}" --cword="$COMP_CWORD" -- "${COMP_WORDS[@]}" ) )
+    COMPREPLY=( $(usage complete-word --shell bash -s "${_usage_spec_mise_2024_12_1}" --cword="$COMP_CWORD" -- "${COMP_WORDS[@]}" ) )
     if [[ $? -ne 0 ]]; then
         unset COMPREPLY
     fi
--- completions/mise.fish
+++ completions/mise.fish
@@ -6,7 +6,7 @@ if ! command -v usage &> /dev/null
     return 1
 end
 
-if ! set -q _usage_spec_mise_2024_12_0
-  set -g _usage_spec_mise_2024_12_0 (mise usage | string collect)
+if ! set -q _usage_spec_mise_2024_12_1
+  set -g _usage_spec_mise_2024_12_1 (mise usage | string collect)
 end
-complete -xc mise -a '(usage complete-word --shell fish -s "$_usage_spec_mise_2024_12_0" -- (commandline -cop) (commandline -t))'
+complete -xc mise -a '(usage complete-word --shell fish -s "$_usage_spec_mise_2024_12_1" -- (commandline -cop) (commandline -t))'
