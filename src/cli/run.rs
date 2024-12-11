507ee27a736b8cd57714a8365fc88855edf62507
jdx <216188+jdx@users.noreply.github.com>
2024-12-10 18:28:26 -0600

    fix: disable filtering by default on `mise run`
    Needed until https://github.com/jdx/demand/issues/68

--- src/cli/run.rs
+++ src/cli/run.rs
@@ -619,8 +619,7 @@ impl Run {
         );
         let mut s = Select::new("Tasks")
             .description("Select a tasks to run")
-            .filterable(true)
-            .filtering(true);
+            .filterable(true);
         for t in tasks.values().filter(|t| !t.hide) {
             s = s.option(DemandOption::new(&t.name).description(&t.description));
         }
