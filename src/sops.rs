7460c6b70a3fd5231ede826f828553b814f7f054
jdx <216188+jdx@users.noreply.github.com>
2024-12-16 06:33:52 -0600

    fix: added MISE_SOPS_ROPS setting

--- src/sops.rs
+++ src/sops.rs
@@ -7,14 +7,16 @@ use rops::cryptography::hasher::SHA512;
 use rops::file::state::EncryptedFile;
 use rops::file::RopsFile;
 use std::env;
+use std::sync::{Mutex, OnceLock};
 
-pub fn decrypt<PT, F>(input: &str, parse_template: PT) -> result::Result<String>
+pub fn decrypt<PT, F>(input: &str, parse_template: PT, format: &str) -> result::Result<String>
 where
     PT: Fn(String) -> result::Result<String>,
     F: rops::file::format::FileFormat,
 {
-    static ONCE: std::sync::Once = std::sync::Once::new();
-    ONCE.call_once(|| {
+    static AGE_KEY: OnceLock<Option<String>> = OnceLock::new();
+    static MUTEX: Mutex<()> = Mutex::new(());
+    let age = AGE_KEY.get_or_init(|| {
         let p = SETTINGS
             .sops
             .age_key_file
@@ -24,9 +26,14 @@ where
             Ok(p) => p,
             Err(e) => {
                 warn!("failed to parse sops age key file: {}", e);
-                return;
+                return None;
             }
         });
+        if let Some(age_key) = &SETTINGS.sops.age_key {
+            if !age_key.is_empty() {
+                return Some(age_key.clone());
+            }
+        }
         if p.exists() {
             if let Ok(raw) = file::read_to_string(p) {
                 let key = raw
@@ -34,19 +41,40 @@ where
                     .lines()
                     .filter(|l| !l.starts_with('#'))
                     .collect::<String>();
-                env::set_var("ROPS_AGE", key);
+                if !key.trim().is_empty() {
+                    return Some(key);
+                }
             }
         }
-        if let Some(age_key) = &SETTINGS.sops.age_key {
-            if !age_key.is_empty() {
-                env::set_var("ROPS_AGE", age_key);
-            }
-        }
+        None
     });
-    let f = input
-        .parse::<RopsFile<EncryptedFile<AES256GCM, SHA512>, F>>()
-        .wrap_err("failed to parse sops file")?;
-    Ok(f.decrypt::<F>()
-        .wrap_err("failed to decrypt sops file")?
-        .to_string())
+    let _lock = MUTEX.lock().unwrap(); // prevent multiple threads from using the same age key
+    let age_env_key = if SETTINGS.sops.rops {
+        "ROPS_AGE"
+    } else {
+        "SOPS_AGE_KEY"
+    };
+    let prev_age = env::var(age_env_key).ok();
+    if let Some(age) = &age {
+        env::set_var(age_env_key, age.trim());
+    }
+    let output = if SETTINGS.sops.rops {
+        input
+            .parse::<RopsFile<EncryptedFile<AES256GCM, SHA512>, F>>()
+            .wrap_err("failed to parse sops file")?
+            .decrypt::<F>()
+            .wrap_err("failed to decrypt sops file")?
+            .to_string()
+    } else {
+        // TODO: this won't work right if sops is coming from mise since tools won't have been loaded
+        // TODO: this obviously won't work on windows
+        cmd!("sops", "--input-type", format, "--output-type", format, "-d", "/dev/stdin").stdin_bytes(input.as_bytes()).read()?
+    };
+
+    if let Some(age) = prev_age {
+        env::set_var(age_env_key, age);
+    } else {
+        env::remove_var(age_env_key);
+    }
+    Ok(output)
 }
