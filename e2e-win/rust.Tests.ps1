8e794426a80c253b4ce47301e911a6b663a52a14
Roland Schaer <roele33@hotmail.com>
2025-01-07 23:19:40 +0100

    fix: mise install rust failed on windows (#3969)

--- e2e-win/rust.Tests.ps1
+++ e2e-win/rust.Tests.ps1
@@ -1,5 +1,13 @@
 
-Describe 'node' {
+Describe 'rust' {
+    It 'installs rust 1.83.0' {
+        $env:MISE_CARGO_HOME = "%TEMP%\.cargo"
+        $env:MISE_RUSTUP_HOME = "%TEMP%\.rustup"
+        mise x rust@1.83.0 -- rustc -V | Should -BeLike "rustc 1.83.0*"
+        Remove-Item Env:MISE_CARGO_HOME
+        Remove-Item Env:MISE_RUSTUP_HOME
+    }
+
     It 'executes rust 1.82.0' {
         mise x rust@1.82.0 -- rustc -V | Should -BeLike "rustc 1.82.0*"
     }
