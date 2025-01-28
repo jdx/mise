183e2586a9854b8398a88743f001e086040b89f6
boris smidt <smidtboris1@gmail.com>
2025-01-28 14:45:34 +0100

    Add args to the task prefix

--- src/task/mod.rs
+++ src/task/mod.rs
@@ -136,6 +136,7 @@ impl Debug for EitherStringOrIntOrBool {
     }
 }
 
+
 impl Task {
     pub fn new(path: &Path, prefix: &Path, config_root: &Path) -> Result<Task> {
         Ok(Self {
@@ -243,7 +244,13 @@ impl Task {
     }
 
     pub fn prefix(&self) -> String {
-        format!("[{}]", self.display_name())
+        let args = if !self.args.is_empty(){
+            self.args.join(" ")
+        } else {
+            "".to_string()
+        };
+
+        format!("[{}{}]", self.display_name(), args)
     }
 
     pub fn run(&self) -> &Vec<String> {
@@ -388,7 +395,7 @@ impl Task {
             ]
         });
         let idx = self
-            .display_name()
+            .prefix()
             .chars()
             .map(|c| c as usize)
             .sum::<usize>()
