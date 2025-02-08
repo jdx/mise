ef4944f2060baa97694c9cee28ce788618e3b289
Risu <79110363+risu729@users.noreply.github.com>
2025-02-08 20:18:10 +1100

    fix(aqua): re-resolve package for all versions

--- src/backend/aqua.rs
+++ src/backend/aqua.rs
@@ -61,6 +61,7 @@ impl Backend for AquaBackend {
                             warn!("[{}] aqua version filter error: {e}", self.ba);
                         }
                     }
+                    let pkg = AQUA_REGISTRY.package_with_version(&self.id, v).ok()?;
                     if let Some(prefix) = &pkg.version_prefix {
                         if let Some(_v) = v.strip_prefix(prefix) {
                             v = _v
