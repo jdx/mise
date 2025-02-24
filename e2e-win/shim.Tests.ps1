973fdde2c389bce0f6b4adb81804ead89a1e872a
qianlongzt <18493471+qianlongzt@users.noreply.github.com>
2025-02-25 06:12:19 +0800

    fix(mise): hardlink, make sure mise on same filesystem

--- e2e-win/shim.Tests.ps1
+++ e2e-win/shim.Tests.ps1
@@ -8,7 +8,6 @@ Describe 'shim_mode' {
 
             mise settings windows_shim_mode $mode
             mise reshim --force
-
         }
 
         $shimPath = Join-Path -Path $env:MISE_DATA_DIR -ChildPath "shims"
@@ -33,10 +32,19 @@ Describe 'shim_mode' {
     }
 
     It 'run on hardlink' {
-        changeShimMode "hardlink"
+        mise settings windows_shim_mode "hardlink"
 
-        mise x go@1.23.3 -- where go
-        mise x go@1.23.3 -- go version | Should -BeLike "go version go1.23.3 windows/*"
+        # make mise is on same filesystem
+        $misePath = (Get-Command -Type Application mise -all | Select-Object -First 1).Source
+        $binPath = (Join-Path -Path $env:MISE_DATA_DIR -ChildPath "bin")
+        $newMisePath = (Join-Path -Path $binPath -ChildPath "mise.exe")
+        New-Item -ItemType Directory -Path $binPath -Force
+        Copy-Item  $misePath $newMisePath
+
+        &$newMisePath reshim --force
+
+        &$newMisePath x go@1.23.3 -- where go
+        &$newMisePath x go@1.23.3 -- go version | Should -BeLike "go version go1.23.3 windows/*"
 
         (Get-Item -Path (Join-Path -Path $shimPath -ChildPath go.exe)).LinkType | Should -Be "HardLink"
     }
