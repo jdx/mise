e9bd775b9f1c71f49e6b553d02c96e1fb2dd1049
qianlongzt <18493471+qianlongzt@users.noreply.github.com>
2025-02-25 08:43:39 +0800

    feat(shim): Windows shim add hardlink & symlink mode (#4409)
    https://github.com/jdx/mise/discussions/4360
    
    add setting `windows_shim_mode`
    1. `file` default, current implement 
    2. `hardlink`, require on same filesystem
    3. `symlink`: Requires Windows Vista or later with admin privileges or
    enabling "Developer Mode" in Windows 10/11.
    
    
    I am not familiar with Rust or the code base here
    
    test on windows 11 24h2 with "Devloper Mode"

---
+++ e2e-win/shim.Tests.ps1
@@ -0,0 +1,55 @@
+Describe 'shim_mode' {
+
+    BeforeAll {
+        function changeShimMode {
+            param (
+                [string]$mode
+            )
+
+            mise settings windows_shim_mode $mode
+            mise reshim --force
+        }
+
+        $shimPath = Join-Path -Path $env:MISE_DATA_DIR -ChildPath "shims"
+    }
+
+    AfterAll {
+        mise settings unset windows_shim_mode
+    }
+
+    It 'run on symlink' {
+        changeShimMode "symlink"
+
+        mise x go@1.23.3 -- where go
+        mise x go@1.23.3 -- go version | Should -BeLike "go version go1.23.3 windows/*"
+        
+        (Get-Item -Path (Join-Path -Path $shimPath -ChildPath go.exe)).LinkType | Should -Be "SymbolicLink"
+    }
+
+    It 'run on file' {
+        changeShimMode "file"
+
+        mise x go@1.23.3 -- where go
+        mise x go@1.23.3 -- go version | Should -BeLike "go version go1.23.3 windows/*"
+
+        (Get-Item -Path  (Join-Path -Path $shimPath -ChildPath go.cmd)).LinkType | Should -Be $null
+    }
+
+    It 'run on hardlink' {
+        mise settings windows_shim_mode "hardlink"
+
+        # make mise is on same filesystem
+        $misePath = (Get-Command -Type Application mise -all | Select-Object -First 1).Source
+        $binPath = (Join-Path -Path $env:MISE_DATA_DIR -ChildPath "bin")
+        $newMisePath = (Join-Path -Path $binPath -ChildPath "mise.exe")
+        New-Item -ItemType Directory -Path $binPath -Force
+        Copy-Item  $misePath $newMisePath
+
+        &$newMisePath reshim --force
+
+        &$newMisePath x go@1.23.3 -- where go
+        &$newMisePath x go@1.23.3 -- go version | Should -BeLike "go version go1.23.3 windows/*"
+
+        (Get-Item -Path (Join-Path -Path $shimPath -ChildPath go.exe)).LinkType | Should -Be "HardLink"
+    }
+}
