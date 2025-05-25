118c7cd307e5ce4cf2008dc2bbac4d3cb69e34bf
Afiq Nazrie <afnazrie@gmail.com>
2025-05-25 22:21:48 +0700

    test(zig)!: change -dev version to -mach

--- e2e-win/zig.Tests.ps1
+++ e2e-win/zig.Tests.ps1
@@ -3,7 +3,7 @@ Describe 'zig' {
         mise x zig@0.13.0 -- zig version | Should -be "0.13.0"
     }
 
-    It 'executes zig 0.14.0-dev.2577+271452d22' {
-        mise x zig@0.14.0-dev.2577+271452d22 -- zig version | Should -be "0.14.0-dev.2577+271452d22"
+    It 'executes zig 2024.11.0-mach' {
+        mise x zig@2024.11.0-mach -- zig version | Should -be "0.14.0-dev.2577+271452d22"
     }
 }
