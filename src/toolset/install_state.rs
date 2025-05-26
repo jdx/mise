076c8ece73854ad3ee6bcbb9b88b3d5c234b514c
Roland Schaer <roele33@hotmail.com>
2025-05-26 11:56:05 +0200

    fix: installing tools with postinstall hooks fails

--- src/toolset/install_state.rs
+++ src/toolset/install_state.rs
@@ -222,10 +222,16 @@ fn read_backend_meta(short: &str) -> Option<Vec<String>> {
 }
 
 pub fn write_backend_meta(ba: &BackendArg) -> Result<()> {
-    // do not write options for core plugins
-    let full = match ba.full().starts_with("core:") {
-        true => ba.full(),
-        false => ba.full_with_opts(),
+    // only use full_with_opts for specific plugin prefixes
+    let full = match ba.full() {
+        full if full.starts_with("cargo:")
+            || full.starts_with("go:")
+            || full.starts_with("pipx:")
+            || full.starts_with("ubi:") =>
+        {
+            ba.full_with_opts()
+        }
+        _ => ba.full(),
     };
     let doc = format!("{}\n{}", ba.short, full);
     file::write(backend_meta_path(&ba.short), doc.trim())?;
