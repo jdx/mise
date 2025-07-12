e4115b2a808ed4fb3027b4722477f5cb36c3c257
jdx <216188+jdx@users.noreply.github.com>
2025-07-12 08:06:23 -0500

    wip

---
+++ crates/vfox/plugins/hooks/pre_install.lua
@@ -0,0 +1,12 @@
+--- Returns some pre-installed information, such as version number, download address, local files, etc.
+--- If checksum is provided, vfox will automatically check it for you.
+--- @param ctx table
+--- @field ctx.version string User-input version
+--- @return table Version information
+function PLUGIN:PreInstall(ctx)
+    local version = ctx.version
+
+    return {
+        version = version,
+    }
+end
