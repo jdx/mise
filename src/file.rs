89d2ae05a8d714342c33222d3bed20dc6079845f
Risu <79110363+risu729@users.noreply.github.com>
2025-07-17 03:37:09 +1000

    Revert "refactor: remove closure"
    This reverts commit 61e20662ef136677602b55144e90765290e5556e.

--- src/file.rs
+++ src/file.rs
@@ -773,7 +773,7 @@ fn strip_archive_path_components(dir: &Path, strip_depth: usize) -> Result<()> {
     let top_level_paths = ls(dir)?;
     let entries: Vec<PathBuf> = top_level_paths
         .iter()
-        .map(ls)
+        .map(|p| ls(p))
         .collect::<Result<Vec<_>>>()?
         .into_iter()
         .flatten()
