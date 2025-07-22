b7c68c3b01293bef8f6b9ac91196ae6b20f6eb9c
Risu <79110363+risu729@users.noreply.github.com>
2025-07-22 13:09:00 +1000

    Revert "chore: debug"
    This reverts commit e594b1e37d354f4ea8156d87c61f467174ee59b6.

--- e2e-win/npm_backend.Tests.ps1
+++ e2e-win/npm_backend.Tests.ps1
@@ -4,9 +4,7 @@ Describe 'npm_backend' {
     }
     It 'installs npm:prettier 3.6.2 with bun' {
         $env:MISE_NPM_BUN = "true"
-        $env:RUST_LOG = "trace"
         mise x node@24.4.1 bun@1.2.19 npm:prettier@3.6.2 -- prettier --version | Should -be "3.6.2"
         Remove-Item Env:MISE_NPM_BUN
-        Remove-Item Env:RUST_LOG
     }
 }
