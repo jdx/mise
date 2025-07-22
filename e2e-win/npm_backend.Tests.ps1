c5972615cb66436d7518764d930a4b417c525fec
Risu <79110363+risu729@users.noreply.github.com>
2025-07-22 10:33:19 +1000

    test: debug

--- e2e-win/npm_backend.Tests.ps1
+++ e2e-win/npm_backend.Tests.ps1
@@ -4,6 +4,12 @@ Describe 'npm_backend' {
     }
     It 'installs npm:prettier 3.6.2 with bun' {
         $env:MISE_NPM_BUN = "true"
+        mise settings npm.bun
+        mise x npm:prettier@3.6.2 -- echo $env:PATH
+        mise x npm:prettier@3.6.2 -- prettier --version
+        mise x node bun npm:prettier@3.6.2 -- echo $env:PATH
+        mise x node bun npm:prettier@3.6.2 -- prettier --version
+        mise x npm:prettier@3.6.2 -- prettier --version | Should -be "3.6.2"
         mise x node bun npm:prettier@3.6.2 -- prettier --version | Should -be "3.6.2"
         Remove-Item Env:MISE_NPM_BUN
     }
