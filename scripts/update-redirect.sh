4e2a3be78b62083e2a69d5add7bdcf69d27a5813
jdx <216188+jdx@users.noreply.github.com>
2025-07-22 04:15:30 -0700

    chore: use 302 redirects for curl installs (#5747)

--- scripts/update-redirect.sh
+++ scripts/update-redirect.sh
@@ -26,7 +26,7 @@ curl --fail-with-body -X PATCH "https://api.cloudflare.com/client/v4/zones/$ZONE
   "action_parameters": {
     "from_value": {
       "preserve_query_string": false,
-      "status_code": 301,
+      "status_code": 302,
       "target_url": {
         "expression": "concat(\"https://github.com/jdx/mise/releases/download/${CURRENT_VERSION}/\", wildcard_replace(http.request.uri.path, \"/mise-latest-*\", \"mise-${CURRENT_VERSION}-\${1}\"))"
       }
