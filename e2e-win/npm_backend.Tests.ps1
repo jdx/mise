b5d3eab62e3b9b88298cd1b21fb6fa2463c46bc9
Risu <79110363+risu729@users.noreply.github.com>
2025-07-22 13:31:54 +1000

    chore: add debug commands in test

--- e2e-win/npm_backend.Tests.ps1
+++ e2e-win/npm_backend.Tests.ps1
@@ -4,6 +4,8 @@ Describe 'npm_backend' {
     }
     It 'installs npm:prettier 3.6.2 with bun' {
         $env:MISE_NPM_BUN = "true"
+        mise x node@24.4.1 bun@1.2.19 npm:prettier@3.6.2 -- where prettier
+        mise x node@24.4.1 bun@1.2.19 npm:prettier@3.6.2 -- ls ~\.local\share\mise\installs\npm-prettier\3.6.2\bin
         mise x node@24.4.1 bun@1.2.19 npm:prettier@3.6.2 -- prettier --version | Should -be "3.6.2"
         Remove-Item Env:MISE_NPM_BUN
     }
