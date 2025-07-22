42d3f48c0129e7ff0b3ef54fa882ff5aa93a05f5
Risu <79110363+risu729@users.noreply.github.com>
2025-07-22 12:40:14 +1000

    test: pin node and bun versions

--- e2e-win/npm_backend.Tests.ps1
+++ e2e-win/npm_backend.Tests.ps1
@@ -1,10 +1,10 @@
 Describe 'npm_backend' {
     It 'installs npm:prettier 3.6.2 with npm' {
-        mise x node npm:prettier@3.6.2 -- prettier --version | Should -be "3.6.2"
+        mise x node@24.4.1 npm:prettier@3.6.2 -- prettier --version | Should -be "3.6.2"
     }
     It 'installs npm:prettier 3.6.2 with bun' {
         $env:MISE_NPM_BUN = "true"
-        mise x node bun npm:prettier@3.6.2 -- prettier --version | Should -be "3.6.2"
+        mise x node@24.4.1 bun@1.2.19 npm:prettier@3.6.2 -- prettier --version | Should -be "3.6.2"
         Remove-Item Env:MISE_NPM_BUN
     }
 }
