a4f50106d0892f61282742b3fc0b17815fff68ff
Risu <79110363+risu729@users.noreply.github.com>
2025-07-22 13:38:53 +1000

    test: use different npm package for test

--- e2e-win/npm_backend.Tests.ps1
+++ e2e-win/npm_backend.Tests.ps1
@@ -2,11 +2,9 @@ Describe 'npm_backend' {
     It 'installs npm:prettier 3.6.2 with npm' {
         mise x node@24.4.1 npm:prettier@3.6.2 -- prettier --version | Should -be "3.6.2"
     }
-    It 'installs npm:prettier 3.6.2 with bun' {
+    It 'installs npm:cowsay 1.6.0 with bun' {
         $env:MISE_NPM_BUN = "true"
-        mise x node@24.4.1 bun@1.2.19 npm:prettier@3.6.2 -- where prettier
-        mise x node@24.4.1 bun@1.2.19 npm:prettier@3.6.2 -- ls ~\.local\share\mise\installs\npm-prettier\3.6.2\bin
-        mise x node@24.4.1 bun@1.2.19 npm:prettier@3.6.2 -- prettier --version | Should -be "3.6.2"
+        mise x node@24.4.1 bun@1.2.19 npm:cowsay@1.6.0 -- cowsay --version | Should -be "1.6.0"
         Remove-Item Env:MISE_NPM_BUN
     }
 }
