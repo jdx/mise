557536974ef5b627ad2ce440369580e2516dd4fb
Risu <79110363+risu729@users.noreply.github.com>
2025-08-13 18:18:17 +1000

    refactor

--- src/cli/config/generate.rs
+++ src/cli/config/generate.rs
@@ -39,7 +39,7 @@ impl ConfigGenerate {
 
     fn tool_versions(&self, tool_versions: &Path) -> Result<String> {
         let to =
-            config_file::parse_or_init(&PathBuf::from(env::MISE_DEFAULT_CONFIG_FILENAME.clone()))?;
+            config_file::parse_or_init(&PathBuf::from(&*env::MISE_DEFAULT_CONFIG_FILENAME))?;
         let from = config_file::parse(tool_versions)?;
         let tools = from.to_tool_request_set()?.tools;
         for (ba, tools) in tools {
