46901e553f40b0702a4d8bed2c55c33b54c8a108
mise-en-dev <release@mise.jdx.dev>
2025-11-23 00:12:09 +0000

    chore: release 2025.11.8

--- completions/mise.bash
+++ completions/mise.bash
@@ -9,7 +9,7 @@ _mise() {
 
 	local cur prev words cword was_split comp_args
     _comp_initialize -n : -- "$@" || return
-    local spec_file="${TMPDIR:-/tmp}/usage__usage_spec_mise_2025_11_7.spec"
+    local spec_file="${TMPDIR:-/tmp}/usage__usage_spec_mise_2025_11_8.spec"
     if [[ ! -f "$spec_file" ]]; then
         mise usage > "$spec_file"
     fi
