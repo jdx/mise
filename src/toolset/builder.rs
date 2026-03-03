dacf2070975b754faff03f0c08c2a1d3dec8b7ff
Mark Lee <mise@lazymalevolence.com>
2026-03-03 15:58:37 -0800

    Don't reference the flag, reference the enum

--- src/toolset/builder.rs
+++ src/toolset/builder.rs
@@ -107,7 +107,7 @@ impl ToolsetBuilder {
 
     fn load_runtime_env(&self, ts: &mut Toolset, env: EnvMap) -> eyre::Result<()> {
         if self.scope == ConfigScope::LocalOnly {
-            // --local excludes env-based tool versions (MISE_*_VERSION).
+            // LocalOnly excludes env-based tool versions (MISE_*_VERSION).
             return Ok(());
         }
         for (k, v) in env {
