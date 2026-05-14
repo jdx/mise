e76e3475c04d9c986b2d1c29819ebe434962697d
Taku Kodma <79110363+risu729@users.noreply.github.com>
2026-05-14 12:25:27 +1000

    chore(vfox): embed secondary mise-plugins plugins

---
+++ crates/vfox/embedded-plugins/vfox-groovy/lib/util.lua
@@ -0,0 +1,34 @@
+
+local util = {}
+
+util.GROOVY_URL = "https://archive.apache.org/dist/groovy"
+util.DOWNLOAD_URL = "https://archive.apache.org/dist/groovy/%s/distribution/apache-groovy-sdk-%s.zip"
+
+function util:compare_versions(v1o, v2o)
+    local v1 = v1o.version
+    local v2 = v2o.version
+    local v1_parts = {}
+    for part in string.gmatch(v1, "[^.]+") do
+        table.insert(v1_parts, tonumber(part))
+    end
+
+    local v2_parts = {}
+    for part in string.gmatch(v2, "[^.]+") do
+        table.insert(v2_parts, tonumber(part))
+    end
+
+    for i = 1, math.max(#v1_parts, #v2_parts) do
+        local v1_part = v1_parts[i] or 0
+        local v2_part = v2_parts[i] or 0
+        if v1_part > v2_part then
+            return true
+        elseif v1_part < v2_part then
+            return false
+        end
+    end
+
+    return false
+end
+
+
+return util
