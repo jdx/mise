9adc3af4ead3bdd9b8c884358aeb2e2abd4bf2b3
Jeff Dickey <216188+jdx@users.noreply.github.com>
2026-07-07 23:05:04 -0500

    registry: use vfox for tiny

--- src/shorthands.rs
+++ src/shorthands.rs
@@ -89,6 +89,7 @@ mod tests {
             shorthands["teleport-community"][0],
             "vfox:jdx/vfox-teleport-community"
         );
+        assert_str_eq!(shorthands["tiny"][0], "vfox:jdx/vfox-tiny");
         assert_str_eq!(shorthands["node"][0], "https://node");
         assert_str_eq!(shorthands["xxxxxx"][0], "https://xxxxxx");
     }
