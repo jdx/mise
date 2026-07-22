629cf58bd7cc6bc1cc8671bc5bf4f57a8cb0018a
default <216188+jdx@users.noreply.github.com>
2026-07-22 22:00:43 +0000

    fix(python): add python3 executable on Windows

--- e2e-win/python.Tests.ps1
+++ e2e-win/python.Tests.ps1
@@ -4,8 +4,15 @@ Describe 'python' {
         $env:MISE_PYTHON_GITHUB_ATTESTATIONS = "0"
     }
 
-    It 'executes python 3.12.0' {
+    It 'executes python and python3 for 3.12.0' {
+        mise install python@3.12.0 --force | Out-Null
+
+        $installPath = (mise where python@3.12.0).Trim()
+        (Join-Path $installPath 'python3.exe') | Should -Exist
+
         mise x python@3.12.0 -- where python
         mise x python@3.12.0 -- python --version | Should -Be "Python 3.12.0"
+        mise x python@3.12.0 -- where python3
+        mise x python@3.12.0 -- python3 --version | Should -Be "Python 3.12.0"
     }
 }
